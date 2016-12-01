751f0c5f02583d88aac06afa3bcae8d255f2499d
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-01 06:11:03 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,29 @@
+import re
+
+dirs = "R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3".split(", ")
+
+direction = 0
+pos_x = 0
+pos_y = 0
+
+for inst in dirs:
+    if inst[0] == 'R':
+        direction = (direction + 1) % 4
+    else:
+        direction = (direction + 3) % 4
+
+    steps = int(inst[1:])
+
+    if (direction & 2) != 0:
+        steps = -steps
+
+    if (direction & 1) == 0:
+        pos_y += steps
+    else:
+        pos_x += steps
+
+print(pos_x)
+print(pos_y)
+print(abs(pos_x) + abs(pos_y))
+        
+        
