788b170ea1c6dc9b8104d311d6db3863737ad01a
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-02 06:24:06 +0100

    Day 2 part 2

--- day2.py
+++ day2.py
@@ -1,19 +1,30 @@
 import re
 import sys
 
-pos_x = 1
-pos_y = 1
+pos_x = -2
+pos_y = 0
+
+table = (
+    "**1**"
+    "*234*"
+    "56789"
+    "*ABC*"
+    "**D**"
+)
+
+def valid(n, other):
+    return abs(n) <= 2 and abs(other) <= 2 - abs(n)
 
 for line in sys.stdin:
     for ch in line:
-        if ch == "U" and pos_y > 0:
+        if ch == "U" and valid(pos_y - 1, pos_x):
             pos_y -= 1
-        elif ch == "D" and pos_y < 2:
+        elif ch == "D" and valid(pos_y + 1, pos_x):
             pos_y += 1
-        elif ch == "L" and pos_x > 0:
+        elif ch == "L" and valid(pos_x - 1, pos_y):
             pos_x -= 1
-        elif ch == "R" and pos_x < 2:
+        elif ch == "R" and valid(pos_x + 1, pos_y):
             pos_x += 1
-    print(pos_y * 3 + pos_x + 1, end='')
+    print(table[(pos_y + 2) * 5 + pos_x + 2], end='')
 
     
