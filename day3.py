47fe1bd3d50bca8c188cfd50a6f90762f5fdcf66
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-03 06:20:07 +0100

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -3,9 +3,27 @@ import sys
 
 count = 0
 
+triangles = []
+num = 0
+accum = []
+
 for line in sys.stdin:
     parts = list(map(int, line.split()))
 
+    if num == 0:
+        accum = [[], [], []]
+
+    for i in range(len(parts)):
+        accum[i].append(parts[i])
+
+    num += 1
+
+    if num == 3:
+        for p in accum:
+            triangles.append(p)
+        num = 0
+
+for parts in triangles:
     good = True
     
     for i in range(len(parts)):
