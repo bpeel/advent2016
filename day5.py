9cee2f940982a8aa6519c787c57ed8bbe1a53e88
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-05 06:27:37 +0100

    Day 5: part 2

--- day5.py
+++ day5.py
@@ -3,21 +3,29 @@ import re
 import hashlib
 
 prefix = 'ugkcyxxp'
-password = ''
+
+password = [" "] * 8
 
 index = 0
+mask = 0
 
-for i in range(8):
-    while True:
-        m = hashlib.md5()
-        m.update(prefix.encode("utf-8"))
-        m.update(str(index).encode("utf-8"))
-        d = m.digest()
+while mask < 255:
+    m = hashlib.md5()
+    m.update(prefix.encode("utf-8"))
+    m.update(str(index).encode("utf-8"))
+    d = m.digest()
 
-        index += 1
+    index += 1
 
-        if d[0:2] == b"\x00\x00" and d[2] & 0xf0 == 0x00:
-            break
+    if d[0:2] != b"\x00\x00" or d[2] & 0xf0 != 0x00:
+        continue
 
-    password += hex(d[2] & 0xf)[2]
-    print(password)
+    pos = d[2] & 0xf
+
+    if pos >= 8 or (mask & (1 << pos)) != 0:
+        continue
+
+    mask |= (1 << pos)
+
+    password[pos] = hex(d[3] >> 4)[2]
+    print("".join(password))
