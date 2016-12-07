e7aafd35dfb325cdaa10d979d7c81dc426140654
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-07 06:40:47 +0100

    Day 7: Both parts

--- day7.py
+++ day7.py
@@ -1,18 +1,17 @@
 import sys
 import re
-from itertools import permutations
-
-count = 0
 
 def has_pair(s):
-    for b in re.finditer(r'(.)(.)\2\1', s):
-        b = b.group(0)
-        if b[0] != b[1]:
-            return True
+    return re.search(r'(.)(?!\1)(.)\2\1', s) != None
 
-    return False
+def has_tls(s):
+    for b in re.finditer(r'\[.*?\]', s):
+        if has_pair(b.group(0)):
+            return False
 
-def valid(s):
+    return has_pair(s)
+
+def has_ssl(s):
     outside = re.sub(r'\[.*?\]', '', s)
 
     for i in range(len(outside) - 2):
@@ -22,9 +21,11 @@ def valid(s):
         for c in re.finditer(r'\[.*?\]', s):
             c = c.group(0)
 
-            if re.search(outside[i + 1] + outside[i] + outside[i + 1], c):
+            if c.find(outside[i + 1] + outside[i] + outside[i + 1]) != -1:
                 return True
 
     return False
 
-print(sum(map(valid, sys.stdin)))
+lines = [line.rstrip() for line in sys.stdin]
+print("Part 1", sum(map(has_tls, lines)))
+print("Part 2", sum(map(has_ssl, lines)))
