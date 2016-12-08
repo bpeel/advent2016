e4b755af77d69762e3c68607d16dbb8e1406e075
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-08 11:01:11 +0100

    Day 8: Video format suitable for twitter

--- day8-video.sh
+++ day8-video.sh
@@ -3,6 +3,6 @@
 set -e
 
 IN_ARGS="-f rawvideo -pixel_format rgb24 -video_size 400x96 -framerate 30"
-OUT_ARGS="-c:v libvpx -b:v 3M"
+OUT_ARGS="-pix_fmt yuv420p -c:v libx264 -b:v 1M -vf pad=400:220:x=0:y=64"
 
-./day8-video < day8.txt | ffmpeg -y $IN_ARGS -i - $OUT_ARGS day8.webm
+./day8-video < day8.txt | ffmpeg -y $IN_ARGS -i - $OUT_ARGS day8.mp4
