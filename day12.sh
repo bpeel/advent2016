6d30e466b5d65bcd50f91540c736900f19db919a
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-12 11:35:13 +0100

    Day 12: Part 2

--- day12.sh
+++ day12.sh
@@ -10,15 +10,17 @@ cat<<EOF > day12.c
 int
 main(int argc, char **argv)
 {
+        int part;
         uint64_t a, b, c, d;
+
+        for (part = 0; part < 2; part++) {
+                printf("Part %i\n", part + 1);
+
+                a = b = d = 0;
+                c = part;
 
         asm("\n"
 EOF
-
-# Clear all of the registers
-for x in a b c d; do
-    echo "\"xor %[$x],%[$x]\n\""
-done >> day12.c
 
 # Each instruction is padded out to 8 bytes with nops so that the jump
 # instructions can just multiply the relative address by 8
@@ -35,16 +37,17 @@ sed -e 's/cpy \([0-9]\+\) \(.\)/mov $\1, %[\2]/' \
 
 cat<<EOF >> day12.c
             ".align 8 ; nop\n"
-            : [a] "=r" (a),
-              [b] "=r" (b),
-              [c] "=r" (c),
-              [d] "=r" (d));
+            : [a] "+r" (a),
+              [b] "+r" (b),
+              [c] "+r" (c),
+              [d] "+r" (d));
         printf("a = %" PRIu64 "\n"
                "b = %" PRIu64 "\n"
                "c = %" PRIu64 "\n"
                "d = %" PRIu64 "\n"
                "\n",
                a, b, c, d);
+        }
         return EXIT_SUCCESS;
 }
 EOF
