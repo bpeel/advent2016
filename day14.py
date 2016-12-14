d729c07a93fc199c25b884ead1bffc5b6fcc1091
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-14 06:20:46 +0100

    Day 14

---
+++ day14.py
@@ -0,0 +1,41 @@
+import hashlib
+import sys
+import re
+from itertools import permutations
+
+salt = "zpqevtbw"
+#salt = "abc"
+
+def get_hash(index):
+    m = hashlib.md5()
+    m.update(salt.encode("utf-8"))
+    m.update(str(index).encode("utf-8"))
+    return m.hexdigest()
+
+hashes = [get_hash(x) for x in range(1001)]
+
+index = 0
+found = 0
+
+triple = re.compile(r'(.)\1\1')
+fivel = re.compile(r'(.)\1\1\1\1')
+
+while True:
+    md = triple.search(hashes.pop(0))
+    if md:
+        fivel = re.compile(md.group(1) * 5)
+        got5 = False
+        for h in hashes:
+            if fivel.search(h):
+                got5 = True
+                break
+        if got5:
+            found += 1
+            print(index, found)
+            if found >= 64:
+                break
+
+    index += 1
+    hashes.append(get_hash(index + len(hashes)))
+
+print(index)
