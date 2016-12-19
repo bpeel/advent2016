ba517213741c903fec492d13dd73d8a54975a048
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-19 13:54:52 +0100

    Day 19: Start search from the last victim
    That makes the search substantially quicker.

--- day19.c
+++ day19.c
@@ -43,23 +43,49 @@ create_elves(int num_elves)
 static void
 solve(int num_elves)
 {
-        struct elf *thief, *victim;
+        struct elf *thief, *victim, *neighbour;
         int victim_pos, i;
+        int neighbour_pos, pos_diff;
 
         thief = create_elves(num_elves);
+        neighbour = thief;
+        /* neighbor_pos is the offset from the thief to the neighbour,
+         * going along next pointers */
+        neighbour_pos = 0;
 
         while (num_elves > 1) {
+                /* Victim pos relative to the thief */
                 victim_pos = num_elves / 2;
-                for (victim = thief, i = 0; i < victim_pos; i++)
-                        victim = victim->next;
+
+                /* Start searching from a neighbour of the previous victim */
+                victim = neighbour;
+                pos_diff = abs(victim_pos) - abs(neighbour_pos);
+
+                if (abs(victim_pos) - abs(neighbour_pos) > num_elves / 2) {
+                        pos_diff = num_elves - pos_diff;
+                        /* Search backwards */
+                        for (i = 0; i < pos_diff; i++)
+                                victim = victim->prev;
+                } else {
+                        /* Search forwards */
+                        for (i = 0; i < pos_diff; i++)
+                                victim = victim->next;
+                }
 
                 thief->presents += victim->presents;
+
+                num_elves--;
+
+                neighbour = victim->next;
+                /* The neighbour pos decreases because the thief is
+                 * about to move forward one */
+                neighbour_pos = victim_pos - 1;
+                if (neighbour_pos < 0)
+                        neighbour_pos = num_elves - 1;
 
                 victim->next->prev = victim->prev;
                 victim->prev->next = victim->next;
                 free(victim);
-
-                num_elves--;
 
                 thief = thief->next;
         }
