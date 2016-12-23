279a0144df76732d30ddc6d961a151aa9c482fd0
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-12-23 06:54:15 +0100

    Day 23: Part 1

--- day23.py
+++ day23.py
@@ -17,15 +17,18 @@ class Machine:
         return self.registers[arg]
 
     def cpy(self, inst):
-        self.registers[inst.args[1]] = self.get_arg(inst.args[0])
+        if isinstance(inst.args[1], str):
+            self.registers[inst.args[1]] = self.get_arg(inst.args[0])
         self.pc += 1
 
     def inc(self, inst):
-        self.registers[inst.args[0]] += 1
+        if isinstance(inst.args[0], str):
+            self.registers[inst.args[0]] += 1
         self.pc += 1
 
     def dec(self, inst):
-        self.registers[inst.args[0]] -= 1
+        if isinstance(inst.args[0], str):
+            self.registers[inst.args[0]] -= 1
         self.pc += 1
 
     def jnz(self, inst):
@@ -34,17 +37,35 @@ class Machine:
         else:
             self.pc += 1
 
+    def tgl(self, inst):
+        inst_num = self.pc + self.get_arg(inst.args[0])
+        if inst_num >= 0 and inst_num < len(self.instructions):
+            other = self.instructions[inst_num]
+            if len(other.args) == 1:
+                if other.opcode == "inc":
+                    other.opcode = "dec"
+                else:
+                    other.opcode = "inc"
+            else:
+                if other.opcode == "jnz":
+                    other.opcode = "cpy"
+                else:
+                    other.opcode = "jnz"
+        self.pc += 1
+                    
     opcodes = {
         "cpy" : cpy,
         "inc" : inc,
         "dec" : dec,
-        "jnz" : jnz
+        "jnz" : jnz,
+        "tgl" : tgl
     }
 
     def execute(self):
         self.pc = 0
-        for reg in "abcd":
+        for reg in "bcd":
             self.registers[reg] = 0
+        self.registers['a'] = 7
 
         while self.pc < len(self.instructions):
             inst = self.instructions[self.pc]
@@ -68,5 +89,5 @@ def get_instruction(line):
 
 machine = Machine([get_instruction(line) for line in sys.stdin])
 machine.execute()
-print(machine.registers)
 
+print("Part 1:", machine.registers['a'])
