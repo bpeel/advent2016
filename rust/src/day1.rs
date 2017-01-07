ce9109c648c25ca566e93056251d41e573638fa9
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-01-07 18:51:52 +0100

    rust: Day 1: Part 2

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,6 +1,18 @@
 use std::io;
 use std::io::BufRead;
 
+#[derive(Copy, Clone)]
+enum Rotation {
+    Left,
+    Right
+}
+
+#[derive(Copy, Clone)]
+struct Instruction {
+    rotation : Rotation,
+    distance : u32
+}
+
 struct Direction {
     x : i32,
     y : i32,
@@ -22,46 +34,85 @@ impl Direction {
     }
 }
 
+#[derive(Copy, Clone)]
 struct Position {
     x : i32,
     y : i32,
+}
+
+struct Person {
+    part : u8,
+    history : Vec<Position>,
+    position : Position,
     direction : Direction
 }
 
-impl Position {
-    fn new() -> Position {
-        Position {
-            x: 0,
-            y: 0,
+impl Person {
+    fn new(part: u8) -> Person {
+        Person {
+            part: part,
+            history: Vec::<Position>::new(),
+            position: Position { x: 0, y: 0 },
             direction: Direction::new()
         }
     }
 
-    fn step(&mut self, distance : i32) {
-        self.x += self.direction.x * distance;
-        self.y += self.direction.y * distance;
+    fn step(&mut self) {
+        self.position.x += self.direction.x;
+        self.position.y += self.direction.y;
     }
 
-    fn follow_instruction(&mut self, instruction : &str) {
-        let mut p = instruction.chars();
-        match p.next().unwrap() {
-            'L' => self.direction = self.direction.left(),
-            'R' => self.direction = self.direction.right(),
-            _ => unreachable!()
+    fn follow_instruction(&mut self, instruction : &Instruction) -> bool {
+        match instruction.rotation {
+            Rotation::Left => self.direction = self.direction.left(),
+            Rotation::Right => self.direction = self.direction.right(),
         }
-        self.step(p.as_str().parse().unwrap());
+
+        for _ in 0..instruction.distance {
+            self.step();
+            if self.part == 1 {
+                for position in &self.history {
+                    if position.x == self.position.x &&
+                        position.y == self.position.y {
+                            return true;
+                        }
+                }
+                self.history.push(self.position);
+            }
+        }
+        return false;
     }
 }
 
 fn main() {
-    let mut pos = Position::new();
     let stdin = io::stdin();
+    let mut instructions = Vec::<Instruction>::new();
 
     for line in stdin.lock().lines() {
         for part in line.unwrap().split(", ") {
-            pos.follow_instruction(part);
+            let mut p = part.chars();
+            let dir = match p.next().unwrap() {
+                'L' => Rotation::Left,
+                'R' => Rotation::Right,
+                _ => unreachable!()
+            };
+
+            let distance : u32 = p.as_str().parse().unwrap();
+
+            instructions.push(Instruction { rotation: dir,
+                                            distance: distance });
         }
     }
 
-    print!("{}\n", pos.x + pos.y);
+    for part in 0..2 {
+        let mut person = Person::new(part);
+        for instruction in &instructions {
+            if person.follow_instruction(&instruction) {
+                break;
+            }
+        }
+        print!("Part {}: {}\n",
+               part + 1,
+               person.position.x + person.position.y);
+    }
 }
