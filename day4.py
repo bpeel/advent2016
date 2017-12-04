368c400771649a60ed602e11ca59d5c974e31e2a
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-12-04 10:10:41 +0100

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -2,19 +2,25 @@
 
 import sys
 
-count = 0
-
-def password_is_valid(password):
+def password_is_valid(password, normalise):
     words = {}
     for word in password.split():
+        if normalise:
+            word = "".join(sorted(word))
         if word in words:
             return False
         words[word] = True
 
     return True
 
-for line in sys.stdin:
-    if password_is_valid(line):
-        count += 1
+count_part1 = 0
+count_part2 = 0
 
-print(count)
+for line in sys.stdin:
+    if password_is_valid(line, False):
+        count_part1 += 1
+    if password_is_valid(line, True):
+        count_part2 += 1
+
+print("Part 1: {}\n"
+      "Part 2: {}".format(count_part1, count_part2))
