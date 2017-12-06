47c2dc47fdde833d4ed5eee1abc5d62b28b41e91
Neil Roberts <bpeeluk@yahoo.co.uk>
2017-12-06 15:10:47 +0100

    Day 5 part 2

--- day5.py
+++ day5.py
@@ -2,15 +2,22 @@
 
 import sys
 
+def execute(jumps, decrease = False):
+    pc = 0
+    steps = 0
+
+    while pc >= 0 and pc < len(jumps):
+        offset = jumps[pc]
+        if decrease and offset >= 3:
+            jumps[pc] -= 1
+        else:
+            jumps[pc] += 1
+        pc += offset
+        steps += 1
+
+    return steps
+
 jumps = [int(line) for line in sys.stdin]
 
-pc = 0
-steps = 0
-
-while pc >= 0 and pc < len(jumps):
-    offset = jumps[pc]
-    jumps[pc] += 1
-    pc += offset
-    steps += 1
-
-print(steps)
+print("Part 1", execute(list(jumps)))
+print("Part 2", execute(list(jumps), True))
