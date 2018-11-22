1df06e12e4a3209146e22bb772237d65390f2faf
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-11-22 18:02:56 +0100

    Day 10 part 2

--- day10.py
+++ day10.py
@@ -11,14 +11,41 @@ def reverse_section(string, start, length):
         string[b] = tmp
 
 
-skip_size = 0
-current_pos = 0
-string = list(range(256))
-lengths = [int(x) for x in sys.stdin.read().split(',')]
+class Hasher:
+    def __init__(self):
+        self.skip_size = 0
+        self.current_pos = 0
+        self.string = list(range(256))
 
-for length in lengths:
-    reverse_section(string, current_pos, length)
-    current_pos += (skip_size + length) % len(string)
-    skip_size += 1
+    def process(self, lengths):
+        for length in lengths:
+            reverse_section(self.string, self.current_pos, length)
+            self.current_pos += (self.skip_size + length) % len(self.string)
+            self.skip_size += 1
 
-print("Part 1: {}".format(string[0] * string[1]))
+
+input_string = sys.stdin.read().strip()
+
+hasher = Hasher()
+hasher.process(int(x) for x in input_string.split(','))
+
+print("Part 1: {}".format(hasher.string[0] * hasher.string[1]))
+
+hasher = Hasher()
+lengths = [ord(c) for c in input_string] + [17, 31, 73, 47, 23]
+
+for i in range(64):
+    hasher.process(lengths)
+
+def hash_all(funcs):
+    i = iter(funcs)
+    result = next(i)
+
+    for v in i:
+        result ^= v
+
+    return result
+
+hash = [hash_all(hasher.string[(i * 16):((i + 1) * 16)]) for i in range(16)]
+hash_str = "".join("{:02x}".format(x) for x in hash)
+print("Part 2: {}".format(hash_str))
