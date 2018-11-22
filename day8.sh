27c3831d24c7d390b2385b1c3d8143af05d7b22c
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-11-22 16:49:32 +0100

    Day 8 part 2

--- day8.sh
+++ day8.sh
@@ -7,15 +7,20 @@ fi
 
 cat <<EOF
 vars = {}
+part2 = None
 def get(name):
     return vars.get(name, 0)
+def set(name, value):
+    global part2
+    if part2 is None or value > part2:
+        part2 = value
+    vars[name] = value
 def inc(name, amount):
-    vars[name] = get(name) + amount
+    set(name, get(name) + amount)
 def dec(name, amount):
-    vars[name] = get(name) - amount
+    set(name, get(name) - amount)
 EOF
 
 sed -r 's/([a-z]+) (inc|dec) (-?[0-9]+) if ([a-z]+) ([!><=]+ -?[0-9]+)/if get("\4") \5: \2("\1", \3)/'
 
-
-echo 'print(max(vars.values()))'
+echo 'print("Part 1: {}\nPart 2: {}".format(max(vars.values()), part2))'
