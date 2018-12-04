783bbb3529cfe9428d15d6343bcf406f5e6514a0
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-12-04 09:45:29 +0100

    Day 4 part 1

---
+++ 2018/day4.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+START_SHIFT_RE = re.compile(r'Guard #([0-9]+) begins shift')
+SLEEP_RE = re.compile(r':([0-9]+)\] falls asleep')
+WAKE_UP_RE = re.compile(r':([0-9]+)\] wakes up')
+
+current_guard = None
+sleep_minute = None
+guards = {}
+
+for line in sorted(sys.stdin):
+    md = START_SHIFT_RE.search(line)
+    if md:
+        current_guard = int(md.group(1))
+        continue
+    md = SLEEP_RE.search(line)
+    if md:
+        sleep_minute = int(md.group(1))
+        continue
+    md = WAKE_UP_RE.search(line)
+    if md:
+        wake_minute = int(md.group(1))
+        try:
+            sleeps = guards[current_guard]
+        except KeyError:
+            sleeps = []
+            guards[current_guard] = sleeps
+        sleeps.append((sleep_minute, wake_minute))
+        continue
+
+def count_sleep_time(sleeps):
+    return sum(b - a for a, b in sleeps)
+
+def find_sleepy_minute(sleeps):
+    minutes = [0] * 60
+    for start, end in sleeps:
+        for m in range(start, end):
+            minutes[m] += 1
+
+    return max(enumerate(minutes), key=lambda x: x[1])[0]
+    
+sleepy_guard = max(guards.items(), key=lambda x: count_sleep_time(x[1]))
+sleepy_minute = find_sleepy_minute(sleepy_guard[1])
+
+print("Part 1: {}".format(sleepy_minute * sleepy_guard[0]))
