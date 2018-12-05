9edf117325349f6a1c00a1ac97814ae7df2c2f2a
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-12-05 17:25:07 +0100

    Day 5: Stack instead of linked list
    I didn’t think of this, I saw it on Reddit :(

--- 2018/day5.c
+++ 2018/day5.c
@@ -5,76 +5,24 @@
 #include <stdio.h>
 #include <limits.h>
 
-struct list_node {
-        char value;
-        uint16_t next;
-        uint16_t prev;
-};
-
-static struct list_node *
-listify(size_t polymer_length,
-        const char *polymer)
-{
-        struct list_node *head = malloc(sizeof *head * (polymer_length + 1));
-
-        for (size_t i = 0; i < polymer_length; i++) {
-                head[i + 1].value = polymer[i];
-                head[i + 1].prev = i;
-                head[i + 1].next = i + 2;
-        }
-
-        head[polymer_length].next = 0;
-        head->value = '*';
-        head->next = 1;
-        head->prev = polymer_length;
-
-        return head;
-}
-
-static void
-remove_node(struct list_node *head,
-            struct list_node *node)
-{
-        head[node->prev].next = node->next;
-        head[node->next].prev = node->prev;
-        node->next = UINT16_MAX;
-        node->prev = UINT16_MAX;
-}
-
 static size_t
 reduce_polymer(size_t polymer_length,
                const char *polymer)
 {
-        struct list_node *head = listify(polymer_length, polymer);
-        uint16_t n = head->next;
+        char *stack = malloc(polymer_length);
+        char *stack_pos = stack;
 
-        /* While n is before the last element */
-        while (n != 0 && head[n].next != 0) {
-                /* Get the next two elements */
-                char a = head[n].value;
-                char b = head[head[n].next].value;
-
-                if (!!isupper(a) != !!isupper(b) && tolower(a) == tolower(b)) {
-                        /* Skip ahead 2 */
-                        n = head[head[n].next].next;
-                        /* Remove the previous 2 */
-                        remove_node(head, head + head[n].prev);
-                        remove_node(head, head + head[n].prev);
-                        /* If we’re not at the beginning of the list
-                         * then go back a step in case that now
-                         * matches its new next node
-                         */
-                        if (head[n].prev != 0)
-                                n = head[n].prev;
-                        polymer_length -= 2;
-                } else {
-                        n = head[n].next;
-                }
+        for (size_t i = 0; i < polymer_length; i++) {
+                if (stack_pos > stack &&
+                    abs(stack_pos[-1] - polymer[i]) == 'a' - 'A')
+                        stack_pos--;
+                else
+                        *(stack_pos++) = polymer[i];
         }
 
-        free(head);
+        free(stack);
 
-        return polymer_length;
+        return stack_pos - stack;
 }
 
 static void
