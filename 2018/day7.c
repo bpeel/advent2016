3a3089c89ac82492f4c983abee8c4710130ab46e
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-12-07 10:28:20 +0100

    Day 7 part 2

--- 2018/day7.c
+++ 2018/day7.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <strings.h>
 #include <string.h>
+#include <limits.h>
 
 #define MAX_STEPS (sizeof (uint32_t) * 8)
 
@@ -22,6 +23,11 @@ struct instructions {
 struct parse_data {
         struct instructions *instructions;
         uint32_t seen_rules;
+};
+
+struct elf {
+        /* The next time point that this elf will be available */
+        int next_free_time;
 };
 
 static void
@@ -74,18 +80,88 @@ read_instructions(struct instructions *instructions,
         }
 }
 
+static int
+get_next_elf(int n_elves,
+             const struct elf *elves)
+{
+        /* Find the elf with the lowest next free time */
+        int lowest_time = INT_MAX;
+        int lowest_index = 0;
+
+        for (int i = 0; i < n_elves; i++) {
+                if (elves[i].next_free_time < lowest_time) {
+                        lowest_time = elves[i].next_free_time;
+                        lowest_index = i;
+                }
+        }
+
+        return lowest_index;
+}
+
+static int
+task_time(int task_num)
+{
+        return task_num + 61;
+}
+
+static int
+find_next_step(uint32_t next_steps,
+               const int *step_available_time)
+{
+        int min_time = INT_MAX;
+        int min_step = 0;
+
+        while (next_steps) {
+                int step = ffs(next_steps) - 1;
+
+                if (step_available_time[step] < min_time) {
+                        min_time = step_available_time[step];
+                        min_step = step;
+                }
+
+                next_steps &= ~(1 << step);
+        }
+
+        return min_step;
+}
+
 static void
 build(const struct instructions *instructions,
-      char *order)
+      char *order,
+      int n_elves,
+      int *elapsed_time_out)
 {
         int order_length = 0;
         uint32_t next_steps = instructions->root_steps;
         uint8_t completed_count[MAX_STEPS];
+        int step_available_time[MAX_STEPS];
+        struct elf elves[n_elves];
+        int current_time = 0;
 
         memset(completed_count, 0, sizeof completed_count);
+        memset(step_available_time, 0, sizeof step_available_time);
+        memset(elves, 0, sizeof (struct elf) * n_elves);
 
         while (next_steps) {
-                int next_step = ffs(next_steps) - 1;
+                int next_step;
+
+                if (n_elves == 1) {
+                        next_step = ffs(next_steps) - 1;
+                } else {
+                        next_step = find_next_step(next_steps,
+                                                   step_available_time);
+                }
+
+                struct elf *next_elf = elves + get_next_elf(n_elves, elves);
+
+                if (next_elf->next_free_time > current_time)
+                        current_time = next_elf->next_free_time;
+                if (step_available_time[next_step] > current_time)
+                        current_time = step_available_time[next_step];
+
+                int completion_time = current_time + task_time(next_step);
+
+                next_elf->next_free_time = completion_time;
 
                 order[order_length++] = next_step + 'A';
                 next_steps &= ~(1 << next_step);
@@ -100,10 +176,22 @@ build(const struct instructions *instructions,
                                 next_steps |= 1 << child;
                         }
                         children &= ~(1 << child);
+
+                        if (step_available_time[child] < completion_time)
+                                step_available_time[child] = completion_time;
                 }
         }
 
         order[order_length++] = '\0';
+
+        int total_time = 0;
+
+        for (int i = 0; i < n_elves; i++) {
+                if (elves[i].next_free_time > total_time)
+                        total_time = elves[i].next_free_time;
+        }
+
+        *elapsed_time_out = total_time;
 }
 
 int
@@ -114,8 +202,13 @@ main(int argc, char **argv)
         read_instructions(&instructions, stdin);
 
         char order[MAX_STEPS + 1];
+        int elapsed_time;
 
-        build(&instructions, order);
+        build(&instructions, order, 1, &elapsed_time);
 
         printf("Part 1: %s\n", order);
+
+        build(&instructions, order, 5, &elapsed_time);
+
+        printf("Part 2: %i\n", elapsed_time);
 }
