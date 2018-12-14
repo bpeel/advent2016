579a6f54c6097532b1a9df97919262167820e944
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-12-14 11:39:19 +0100

    Day 14: generator

--- 2018/day14.py
+++ 2018/day14.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
 import sys
+import itertools
 
-def get_next_ten(after):
+def recipes():
     history = [3, 7]
+    for n in history:
+        yield n
     elf1 = 0
     elf2 = 1
 
-    while len(history) < after + 10:
+    while True:
         r1 = history[elf1]
         r2 = history[elf2]
         new_sum = r1 + r2
 
         if new_sum > 9:
             history.append(new_sum // 10)
+            yield history[-1]
         history.append(new_sum % 10)
+        yield history[-1]
 
         elf1 = (elf1 + r1 + 1) % len(history)
         elf2 = (elf2 + r2 + 1) % len(history)
-
-    return "".join(str(x) for x in history[after : after + 10])
 
 if len(sys.argv) != 2:
     print("usage: {} <n_recipes>", file=sys.stderr)
     sys.exit(1)
 
 n_recipes = int(sys.argv[1])
-print("Part 1: {}".format(get_next_ten(n_recipes)))
+part1_iterator = itertools.islice(recipes(), n_recipes, n_recipes + 10)
+part1 = "".join(str(x) for x in part1_iterator)
+
+print("Part 1: {}".format(part1))
