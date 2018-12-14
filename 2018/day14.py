d1f8f671b980013f3c56e5ab1b7289f3929326ed
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-12-14 11:56:57 +0100

    Day 14 part 2

--- 2018/day14.py
+++ 2018/day14.py
@@ -2,6 +2,7 @@
 
 import sys
 import itertools
+import collections
 
 def recipes():
     history = [3, 7]
@@ -24,6 +25,32 @@ def recipes():
         elf1 = (elf1 + r1 + 1) % len(history)
         elf2 = (elf2 + r2 + 1) % len(history)
 
+def collection_equal(a, b):
+    bit = iter(b)
+    for x in a:
+        try:
+            if x != next(bit):
+                return False
+        except StopIteration:
+            return False
+
+    try:
+        next(bit)
+    except StopIteration:
+        return True
+
+    return False
+
+def find_in_recipes(to_find):
+    tail = collections.deque()
+    for i, recipe in enumerate(recipes()):
+        if len(tail) >= len(to_find):
+            tail.popleft()
+        tail.append(recipe)
+
+        if collection_equal(to_find, tail):
+            return i - len(to_find) + 1
+
 if len(sys.argv) != 2:
     print("usage: {} <n_recipes>", file=sys.stderr)
     sys.exit(1)
@@ -33,3 +60,5 @@ part1_iterator = itertools.islice(recipes(), n_recipes, n_recipes + 10)
 part1 = "".join(str(x) for x in part1_iterator)
 
 print("Part 1: {}".format(part1))
+
+print("Part 2: {}".format(find_in_recipes([int(x) for x in sys.argv[1]])))
