d378fd8860a171a2b72fcf29b8f9373f99704e9b
Neil Roberts <bpeeluk@yahoo.co.uk>
2018-12-17 22:25:58 +0100

    Day 17: another broken answer

--- 2018/day17.py
+++ 2018/day17.py
@@ -9,6 +9,7 @@ CLAY_RE = re.compile(r'([xy])=([0-9]+), ([xy])=([0-9]+)\.\.([0-9]+)$')
 class Terrain:
     def __init__(self, lines):
         self.clay = set()
+        self.can_settle_cache = dict()
 
         for line in lines:
             md = CLAY_RE.match(line)
@@ -29,56 +30,68 @@ class Terrain:
                 self.clay.add(coord)
 
         self.max_y = max(coord[1] for coord in self.clay)
-        self.visited = dict()
 
     def is_clay(self, x, y):
         return (x, y) in self.clay
 
-    def count_water(self, x, y):
-        ret = self.count_water_i(x, y)
-        print("{},{}: {}".format(x, y, ret))
-        return ret
-
-    def count_water_i(self, x, y):
+    def can_settle(self, x, y):
         try:
-            return (0, self.visited[(x, y)])
+            return self.can_settle_cache[(x, y)]
         except KeyError:
-            pass
+            ret = self._can_settle(x, y)
+            self.can_settle_cache[(x, y)] = ret
+            return ret
 
+    def _can_settle(self, x, y):
         if y > self.max_y:
-            return (0, False)
+            return False
 
         if self.is_clay(x, y):
-            return (0, True)
+            return True
 
-        clay_count, below_settle = self.count_water(x, y + 1)
+        if not self.can_settle(x, y + 1):
+            return False
 
-        clay_count += 1
-
-        self.visited[(x, y)] = below_settle
-
-        if not below_settle:
-            return (clay_count, False)
-
-        can_settle = True
-
-        for iterator in (itertools.count(x - 1, -1),
-                         itertools.count(x + 1, +1)):
-            for xx in iterator:
+        for step in (-1, 1):
+            for xx in itertools.count(x + step, step):
                 if self.is_clay(xx, y):
                     break
+                if not self.can_settle(xx, y + 1):
+                    return False
 
-                (below_count, below_settle) = self.count_water(xx, y + 1)
+        return True
 
-                clay_count += 1 + below_count
+class Counter:
+    def __init__(self, terrain):
+        self.terrain = terrain
+        self.water = set()
 
-                if not below_settle:
-                    can_settle = False
-                    break
+    def add_water(self, x, y):
+        if y > self.terrain.max_y:
+            return
 
-        return (clay_count, can_settle)
+        if self.terrain.is_clay(x, y):
+            return
 
+        if (x, y) in self.water:
+            return
 
-terrain = Terrain(sys.stdin)
+        self.water.add((x, y))
 
-print("Part 1: {}".format(terrain.count_water(500, 0)[0]))
+        self.add_water(x, y + 1)
+
+        if self.terrain.can_settle(x, y + 1):
+            for step in (-1, 1):
+                self.add_water(x + step, y)
+
+sys.setrecursionlimit(10000)
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as f:
+        terrain = Terrain(f)
+else:
+    terrain = Terrain(sys.stdin)
+counter = Counter(terrain)
+counter.add_water(500, 1)
+
+print("Part 1: {}".format(len(counter.water)))
