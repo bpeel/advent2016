3fdcdf56ffd69460b88edb888ad3845cad78ae19
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-12-02 10:41:28 +0100

    Day 1: part 2

--- 2019/day1.py
+++ 2019/day1.py
@@ -5,6 +5,23 @@ import sys
 def fuel_for_mass(mass):
     return mass // 3 - 2
 
-fuel_sum = sum(fuel_for_mass(int(line.rstrip())) for line in sys.stdin)
+def all_fuel_for_mass(mass):
+    total = 0
 
-print(fuel_sum)
+    while True:
+        mass = fuel_for_mass(mass)
+
+        if mass <= 0:
+            return total
+
+        total += mass
+
+masses = list(int(line.rstrip()) for line in sys.stdin)
+
+part1 = sum(fuel_for_mass(mass) for mass in masses)
+
+print("Part 1: {}".format(part1))
+
+part2 = sum(all_fuel_for_mass(mass) for mass in masses)
+
+print("Part 2: {}".format(part2))
