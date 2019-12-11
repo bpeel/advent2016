000c9f37b5321ac7b71fb5fb8801a05d421e1b5e
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-12-11 17:55:25 +0100

    Day 8: Part 2

--- 2019/day8.py
+++ 2019/day8.py
@@ -19,3 +19,24 @@ best_layer = min(layers, key=lambda x: count_digits(x, 0))
 
 print("Part 1: {}".format(count_digits(best_layer, 1) *
                           count_digits(best_layer, 2)))
+
+def get_pixel(layers, pos):
+    for layer in layers:
+        if layer[pos] != 2:
+            return layer[pos]
+
+    return 2
+
+image = [get_pixel(layers, x) for x in range(WIDTH * HEIGHT)]
+
+with open("day8-part2.ppm", "wb") as f:
+    f.write(("P6\n"
+             "{} {}\n"
+             "255\n").format(WIDTH, HEIGHT).encode("utf-8"))
+    for pixel in image:
+        if pixel == 1:
+            p = b'\xff\xff\xff'
+        else:
+            p = b'\x00\x00\x00'
+
+        f.write(p)
