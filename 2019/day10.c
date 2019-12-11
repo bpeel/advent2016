15e63847c53138d5c3e8f1d3bf4a1fa073171bab
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-12-11 22:57:30 +0100

    Day 10: Part 2

--- 2019/day10.c
+++ 2019/day10.c
@@ -172,6 +172,126 @@ count_visible(size_t n_asteroids,
         return n_asteroids - blocked_count - 1;
 }
 
+static int
+get_quandrant(const struct asteroid *a)
+{
+        if (a->dir_x == 0) {
+                if (a->dir_y <= 0)
+                        return 0;
+                else
+                        return 2;
+        } else if (a->dir_y == 0) {
+                if (a->dir_x > 0)
+                        return 1;
+                else
+                        return 3;
+        } else if (a->dir_x > 0) {
+                if (a->dir_y > 0)
+                        return 1;
+                else
+                        return 0;
+        } else {
+                if (a->dir_y > 0)
+                        return 2;
+                else
+                        return 3;
+        }
+}
+
+static int
+compare_angle_distance(const void *pa,
+                       const void *pb)
+{
+        const struct asteroid *a = pa;
+        const struct asteroid *b = pb;
+
+        if (a->dir_x == b->dir_x && a->dir_y == b->dir_y)
+                return a->mult - b->mult;
+
+        if (a->mult == 0)
+                return -1;
+        if (b->mult == 0)
+                return 1;
+
+        int qa = get_quandrant(a);
+        int qb = get_quandrant(b);
+
+        if (qa != qb)
+                return qa - qb;
+
+        if (a->dir_x == 0 || a->dir_y == 0) {
+                if (b->dir_x == 0 || b->dir_y == 0)
+                        return 0;
+                else
+                        return -1;
+        } else if (b->dir_x == 0 || b->dir_y == 0) {
+                return 1;
+        }
+
+        int acx = a->dir_x * b->dir_y;
+        int bcx = b->dir_x * a->dir_y;
+
+        return bcx - acx;
+}
+
+static void
+part2(size_t n_asteroids,
+      const struct xy_pos *positions,
+      const struct xy_pos *base)
+{
+        struct asteroid *asteroids = get_asteroids(n_asteroids,
+                                                   positions,
+                                                   base);
+
+        /* Sort the asteroids by angle than distance */
+
+        qsort(asteroids,
+              n_asteroids,
+              sizeof *asteroids,
+              compare_angle_distance);
+
+        assert(asteroids[0].mult == 0 &&
+               asteroids[0].dir_x == 0 &&
+               asteroids[0].dir_y == 0);
+
+        int destroyed_count = 0;
+        int pos = 0;
+
+        while (true) {
+                /* Skip to the next unblocked asteroid */
+                while (asteroids[pos].blocked)
+                        pos = (pos + 1) % n_asteroids;
+
+                struct asteroid *a = asteroids + pos;
+
+                a->blocked = true;
+
+                if (++destroyed_count >= n_asteroids)
+                        break;
+
+                if (destroyed_count >= 201) {
+                        int x = a->dir_x * a->mult + base->x;
+                        int y = a->dir_y * a->mult + base->y;
+
+                        printf("Part 2: %i (%i,%i)\n",
+                               x * 100 + y,
+                               x,
+                               y);
+                        break;
+                }
+
+                /* Skip all of the asteroids that have the same
+                 * direction
+                 */
+                do
+                        pos = (pos + 1) % n_asteroids;
+                while (asteroids[pos].dir_x == a->dir_x &&
+                       asteroids[pos].dir_y == a->dir_y);
+        }
+
+        pcx_free(asteroids);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -193,13 +313,14 @@ main(int argc, char **argv)
                 }
         }
 
-        pcx_free(positions);
-
         printf("Part 1: %i (%i,%i)\n",
                most_visible,
                positions[best_asteroid].x,
                positions[best_asteroid].y);
 
+        part2(n_asteroids, positions, positions + best_asteroid);
+
+        pcx_free(positions);
 
         return 0;
 }
