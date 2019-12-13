39ff43ac905d214fd24c5ce9ca1deba3ab864fe5
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-12-13 14:58:12 +0100

    Day 13: Part 2

--- 2019/day13.c
+++ 2019/day13.c
@@ -10,7 +10,25 @@ struct game {
         struct grid *grid;
         int output_num;
         int out_x, out_y;
+        int ball_x, bat_x;
+        int64_t score;
 };
+
+static bool
+input_cb(void *user_data,
+         int64_t *value)
+{
+        struct game *game = user_data;
+
+        if (game->ball_x < game->bat_x)
+                *value = -1;
+        else if (game->ball_x > game->bat_x)
+                *value = 1;
+        else
+                *value = 0;
+
+        return true;
+}
 
 static bool
 output_cb(void *user_data,
@@ -26,6 +44,12 @@ output_cb(void *user_data,
                 game->out_y = value;
                 break;
         case 2:
+                if (game->out_x == -1 && game->out_y == 0)
+                        game->score = value;
+                else if (value == 3)
+                        game->bat_x = game->out_x;
+                else if (value == 4)
+                        game->ball_x = game->out_x;
                 grid_write(game->grid, game->out_x, game->out_y, value);
                 break;
         }
@@ -69,24 +93,33 @@ main(int argc, char **argv)
 
         int ret = EXIT_SUCCESS;
 
-        struct game game = { .grid = grid_new() };
-        struct intcode *machine = intcode_new(memory_size, memory);
+        for (int part = 1; part <= 2; part++) {
+                struct game game = { .grid = grid_new() };
+                struct intcode *machine = intcode_new(memory_size, memory);
 
-        intcode_set_output_function(machine, output_cb, &game);
+                intcode_set_input_function(machine, input_cb, &game);
+                intcode_set_output_function(machine, output_cb, &game);
 
-        struct pcx_error *error = NULL;
+                if (part == 2)
+                        intcode_write(machine, 0, 2, NULL);
 
-        if (intcode_run(machine, &error)) {
-                printf("Part 1: %i\n", count_tiles(game.grid, 2));
-                grid_dump(game.grid, "day13-part1.ppm");
-        } else {
-                fprintf(stderr, "%s\n", error->message);
-                pcx_error_free(error);
-                ret = EXIT_FAILURE;
+                struct pcx_error *error = NULL;
+
+                if (intcode_run(machine, &error)) {
+                        int result = (part == 1 ? count_tiles(game.grid, 2) :
+                                      part == 2 ? game.score :
+                                      0);
+
+                        printf("Part %i: %i\n", part, result);
+                } else {
+                        fprintf(stderr, "Part %i: %s\n", part, error->message);
+                        pcx_error_free(error);
+                        ret = EXIT_FAILURE;
+                }
+
+                intcode_free(machine);
+                grid_free(game.grid);
         }
-
-        intcode_free(machine);
-        grid_free(game.grid);
 
         pcx_free(memory);
 
