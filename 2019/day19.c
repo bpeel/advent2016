b40ffe095b51f76123d4be86c01bcc2f98e770e3
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-12-20 14:03:06 +0100

    Day 19: part 2: first attempt

--- 2019/day19.c
+++ 2019/day19.c
@@ -6,6 +6,8 @@
 #include "read-memory.h"
 #include "pcx-error.h"
 
+#define SHIP_SIZE 100
+
 struct query_data {
         int x, y;
         int input_num;
@@ -19,6 +21,7 @@ day19_error;
 enum day19_error {
         DAY19_ERROR_NO_RESULT,
         DAY19_ERROR_INVALID_RESULT,
+        DAY19_ERROR_FAILED,
 };
 
 static bool
@@ -123,6 +126,100 @@ part1(size_t memory_size,
         return true;
 }
 
+static bool
+advance(size_t memory_size,
+        const int64_t *memory,
+        bool x_first,
+        int *x_in_out, int *y_in_out,
+        struct pcx_error **error)
+{
+        int x = *x_in_out, y = *y_in_out;
+
+        if (x_first)
+                x++;
+        else
+                y++;
+
+        for (int i = 0; i < 100; i++) {
+                bool lit;
+
+                if (!query_program(memory_size,
+                                   memory,
+                                   x, y,
+                                   &lit,
+                                   error))
+                        return false;
+
+                if (lit)
+                        goto found;
+
+                if (x_first)
+                        y++;
+                else
+                        x++;
+        }
+
+        if (x_first)
+                y = *y_in_out;
+        else
+                x = *x_in_out;
+
+found:
+        *x_in_out = x;
+        *y_in_out = y;
+
+        return true;
+}
+
+static bool
+part2(size_t memory_size,
+      const int64_t *memory,
+      int *result,
+      struct pcx_error **error)
+{
+        int top_x = 0, top_y = 0;
+        int bottom_x = 0, bottom_y = 0;
+        int ship_w = 1, ship_h = 1;
+
+        do {
+                if (ship_w > ship_h && ship_w < SHIP_SIZE) {
+                        if (!advance(memory_size,
+                                     memory,
+                                     true,
+                                     &top_x, &top_y,
+                                     error))
+                                return false;
+                } else if (!advance(memory_size,
+                                    memory,
+                                    false,
+                                    &bottom_x, &bottom_y,
+                                    error)) {
+                        return false;
+                }
+
+                ship_w = top_x - bottom_x + 1;
+                ship_h = top_y - bottom_y + 1;
+
+                printf("%i,%i  %i,%i  %i,%i\n",
+                       top_x, top_y,
+                       bottom_x, bottom_y,
+                       ship_w, ship_h);
+
+                if (ship_w > SHIP_SIZE || ship_h > SHIP_SIZE) {
+                        pcx_set_error(error,
+                                      &day19_error,
+                                      DAY19_ERROR_FAILED,
+                                      "Couldn’t find exact ship "
+                                      "size");
+                        return false;
+                }
+        } while (ship_w < SHIP_SIZE || ship_h < SHIP_SIZE);
+
+        *result = bottom_x * 10000 + top_y;
+
+        return true;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -149,6 +246,15 @@ main(int argc, char **argv)
                 printf("Part 1: %i\n", result);
         } else {
                 fprintf(stderr, "Part 1: %s\n", error->message);
+                pcx_error_free(error);
+                error = NULL;
+                ret = EXIT_FAILURE;
+        }
+
+        if (part2(memory_size, memory, &result, &error)) {
+                printf("Part 2: %i\n", result);
+        } else {
+                fprintf(stderr, "Part 2: %s\n", error->message);
                 pcx_error_free(error);
                 error = NULL;
                 ret = EXIT_FAILURE;
