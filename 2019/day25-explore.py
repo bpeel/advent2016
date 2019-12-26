e8f0253c5604372341664d769f24fdc7605e0c1f
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-12-26 23:13:27 +0100

    day25: Build the list of bad items on the fly

--- 2019/day25-explore.py
+++ 2019/day25-explore.py
@@ -8,6 +8,16 @@ import io
 
 Room = collections.namedtuple('Room', ['name', 'exits', 'items'])
 
+OPPOSITE_DIR = {
+    "north": "south",
+    "south": "north",
+    "east": "west",
+    "west": "east"
+}
+
+class NoCommandException(Exception):
+    pass
+
 def read_room(fin):
     room_name = None
     items = []
@@ -54,10 +64,77 @@ def send(prog, command):
     print(command, file=prog.stdin)
 
 def wait_command(prog):
+    count = 0
+
     for line in prog.stdout:
         print(line.rstrip())
         if line.startswith("Command?"):
-            break
+            return
+
+        count += 1
+
+        if count > 100:
+            raise NoCommandException("Too many lines with "
+                                     "no command")
+
+    raise NoCommandException("End of program reached while "
+                             "waiting for prompt")
+
+def get_all_items(prog, bad_items):
+    room = read_room(prog.stdout)
+    rooms = {room.name : room}
+    stack = [(room, 0)]
+    items = []
+    route = None
+
+    while True:
+        room, dir_to_try = stack.pop()
+
+        if room.name == "Security Checkpoint":
+            route = [r[0].exits[r[1] - 1] for r in stack]
+
+        for item in room.items:
+            if item in bad_items or item in items:
+                continue
+            send(prog, "take {}".format(item))
+            try:
+                wait_command(prog)
+            except NoCommandException:
+                print("** Couldn’t pick up {}, restarting".format(item))
+                bad_items.add(item)
+                return None
+
+            items.append(item)
+
+        for d in range(dir_to_try, len(room.exits)):
+            send(prog, room.exits[d])
+            next_room = read_room(prog.stdout)
+            if next_room.name is None:
+                print("** Couldn’t move. Assuming the fault is “{}” and "
+                      "restarting".format(items[-1]))
+                bad_items.add(items[-1])
+                return None
+            if next_room.name in rooms:
+                print("** already been in {}".format(next_room.name))
+                send(prog, OPPOSITE_DIR[room.exits[d]])
+                bt_room = read_room(prog.stdout)
+                assert(bt_room.name == room.name)
+            else:
+                rooms[next_room.name] = next_room
+                stack.append((room, d + 1))
+                stack.append((next_room, 0))
+                break
+        else:
+            if len(stack) < 1:
+                break
+            parent_room, last_dir = stack[-1]
+            print("** backtracking to {}".format(parent_room.name))
+            assert(last_dir > 0)
+            send(prog, OPPOSITE_DIR[parent_room.exits[last_dir - 1]])
+            bt_room = read_room(prog.stdout)
+            assert(bt_room.name == parent_room.name)
+
+    return route, items
 
 def try_all_combos(prog, items):
     last_state = (1 << len(items)) - 1
@@ -83,69 +160,23 @@ def try_all_combos(prog, items):
         if room.name != "Security Checkpoint":
             return
 
-prog = subprocess.Popen(["./build/runner", "-a", "day25-input.txt"],
-                        stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        bufsize=1,
-                        text=True,
-                        encoding="utf-8")
-
-room = read_room(prog.stdout)
-rooms = {room.name : room}
-stack = [(room, 0)]
-bad_items = set(["molten lava",
-                 "infinite loop",
-                 "escape pod",
-                 "photons",
-                 "giant electromagnet"])
-items = []
-route = None
-
-opposite_dir = {
-    "north": "south",
-    "south": "north",
-    "east": "west",
-    "west": "east"
-}
+bad_items = set()
 
 while True:
-    room, dir_to_try = stack.pop()
+    prog = subprocess.Popen(["./build/runner", "-a", "day25-input.txt"],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            bufsize=1,
+                            text=True,
+                            encoding="utf-8")
 
-    if room.name == "Security Checkpoint":
-        route = [r[0].exits[r[1] - 1] for r in stack]
+    result = get_all_items(prog, bad_items)
 
-    for item in room.items:
-        if item in bad_items or item in items:
-            continue
-        send(prog, "take {}".format(item))
-        wait_command(prog)
-        items.append(item)
+    if result is not None:
+        route, items = result
+        break
 
-    for d in range(dir_to_try, len(room.exits)):
-        send(prog, room.exits[d])
-        next_room = read_room(prog.stdout)
-        if next_room.name in rooms:
-            print("** already been in {}".format(next_room.name))
-            send(prog, opposite_dir[room.exits[d]])
-            bt_room = read_room(prog.stdout)
-            assert(bt_room.name == room.name)
-        else:
-            rooms[next_room.name] = next_room
-            stack.append((room, d + 1))
-            stack.append((next_room, 0))
-            break
-    else:
-        if len(stack) < 1:
-            break
-        parent_room, last_dir = stack[-1]
-        print("** backtracking to {}".format(parent_room.name))
-        assert(last_dir > 0)
-        send(prog, opposite_dir[parent_room.exits[last_dir - 1]])
-        bt_room = read_room(prog.stdout)
-        assert(bt_room.name == parent_room.name)
-
-print(list(rooms.values()))
-print(items)
+    prog.kill()
 
 for d in route:
     send(prog, d)
