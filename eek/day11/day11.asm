f882021957677607ebeb54dcfee0cf2ffa4a405f
Neil Roberts <bpeeluk@yahoo.co.uk>
2020-12-11 15:16:36 +0100

    day11: Use the screen buffer directly as the grid memory

--- eek/day11/day11.asm
+++ eek/day11/day11.asm
@@ -2,6 +2,21 @@
 
         * = $1910
 
+        GRID_POS = $70          ; address of byte we’re looking at in the grid
+        XPOS = $72
+        YPOS = $73
+        FILENO = $74
+        TEMP = $80
+
+        SCREEN_START = $3000
+
+        ;; set mode 1. This will also clear the grid (in the screen memory)
+        ;; to zero.
+        lda #VDUMODE
+        jsr OSWRCH
+        lda #2
+        jsr OSWRCH
+
         ldx #<filename
         ldy #>filename
         lda #$40                ; open
@@ -17,38 +32,34 @@ gotfile:
 
         ;; store the file descriptor in Y so we can use it for OS calls
         tay
+        ;; also store it for later in case we need to use Y for something
+        sty FILENO
 
         ;; load the data
         .(
-        XPOS = $70
         lda #0
         sta XPOS
-
-        GRID_POS = $71          ; counter for where to store the next pos
-        lda #<grid
+        sta YPOS
+        lda #<SCREEN_START
         sta GRID_POS
-        lda #>grid
+        lda #>SCREEN_START
         sta GRID_POS + 1
 
-        FILENO = $73
-        sty FILENO
-
-        ldx #0                  ; offset from GRID_POS
+        ldx #0                  ; set X to zero so we can use indirect addr
 
 loop:   
         jsr OSBGET              ; get the next byte of input
         cmp #10
-        bne not_eol
-        jsr finish_line
-        jmp loop
-not_eol:
+        beq eol
         cmp #$fe
         bcs finish_data
         
-        ;; if the byte is "L" (0x4C) we want 2, otherwise if "."
-	;; (0x2e). Let’s just invert bit 1 and use that.
+        ;; if the byte is "L" (0x4C) we want 4, otherwise if "."
+        ;; (0x2e) we want 0. Let’s just grab an inverted bit 1 and put
+        ;; it in bit 2
         eor #2
         and #2
+        asl
 
         ;; are we on an odd X-pos?
         lsr XPOS
@@ -56,86 +67,68 @@ not_eol:
 
         rol XPOS
         asl
-        asl
-        asl
-        asl
         ora (GRID_POS, x)
         sta (GRID_POS, x)
         inc XPOS
-        inc GRID_POS
-        bne loop
-        inc GRID_POS + 1
+        lda GRID_POS
+        adc #8
+        sta GRID_POS
         bcc loop
+        inc GRID_POS + 1
+        bcs loop                ; always jump to loop
 
 evenpos:
         asl XPOS
         sta (GRID_POS, x)
         inc XPOS
-        bne loop
+        bne loop                ; always jump to loop
 
-finish_line:
-        lsr XPOS
-        bcc finish_even
-        inc GRID_POS
-        bne finish_even
-        inc GRID_POS + 1
-finish_even:    
-        lda #64
-        sec
-        sbc XPOS
-        beq done_line
-
-        tay
-        dey
-        lda #0
-
-        .(
-line_loop:
-        sta (GRID_POS, x)
-        inc GRID_POS
-        bne not_high
-        inc GRID_POS + 1
-not_high:       
-        dey
-        bpl line_loop
-        .)
-
-done_line:
+eol:
         lda #0
         sta XPOS
-        ldy FILENO
-        rts
+        inc YPOS
+        jsr get_grid_y
+        jmp loop
 
 finish_data:
-        jsr finish_line
-        lda GRID_POS + 1
-        cmp #>GRID_END
-        bcc finish_data
-        lda GRID_POS
-        cmp #<GRID_END
-        bcc finish_data
-        .)
-
         lda #0
         jsr OSFIND              ; close the file
 
         rts
+        .)
+
+get_grid_y:
+        ;; Set GRID_POS to the start of the memory for the row at YPOS
+        .(
+        lda #0
+        sta GRID_POS
+
+        lda YPOS
+        ;; divide by 8 then multiply by $200 = YPOS&~7 << 9
+        and #$f8
+        lsr
+        lsr
+        sta TEMP
+        adc #>SCREEN_START
+        sta GRID_POS + 1
+
+        lda TEMP
+        ;; divide by 16 to get high byte of (floor(YPOS / 8) * $80)
+        lsr
+        lsr
+        ;; store the carry as the high bit of the low byte
+        ror GRID_POS
+        ;; add the calculated high byte
+        adc GRID_POS + 1
+        sta GRID_POS + 1
+
+        ;; add the ypos % 8
+        lda YPOS
+        and #7
+        adc GRID_POS
+        sta GRID_POS
+        rts
+        .)
         
 filename:
         .byt "data", 13
-
-grid:
-        ;; Space after the program for the grid of data
-
-        ;; This is stored as a grid of 64x128 bytes where each byte
-	;; stores two spaces on the boat. Each space takes up 4 bits
-	;; where the lower two bits represent the current state and the
-	;; upper bits represent the next state in the process of being
-	;; calculated.
-
-        ;; The values stored in the two bits for a space are:
-        ;; 00 - No chair
-        ;; 01 - (Not used)
-        ;; 10 - Unoccupied chair
-        ;; 11 - Occupied chair
-        GRID_END = grid + 64 * 128
