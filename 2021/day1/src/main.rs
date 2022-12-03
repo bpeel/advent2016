d50528f945a22adb0c23dc77d9a0d2bdd7b6f075
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-03 16:03:52 +0100

    2021/1/1: Store the data in a vec

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let mut nums = std::io::stdin().lines().map(|line| {
+    let nums: Vec<u32> = std::io::stdin().lines().map(|line| {
         line.unwrap().trim_end().parse::<u32>().unwrap()
-    });
+    }).collect();
 
-    let first = nums.next().unwrap();
+    let first = nums[0];
 
-    let (_, part1): (u32, u32) = nums.fold((first, 0),
-                                           |(prev, count), next|
-                                           (next,
-                                            count + (next > prev) as u32));
+    let (_, part1): (u32, u32) =
+        nums.iter().skip(1).fold((first, 0),
+                                 |(prev, count), &next|
+                                 (next, count + (next > prev) as u32));
 
     println!("part 1: {}", part1);
 }
