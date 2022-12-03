70c32e29da52484d018e031442dfc025fbdde1d6
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-03 16:17:34 +0100

    2021/1/2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,8 +1,4 @@
-fn main() {
-    let nums: Vec<u32> = std::io::stdin().lines().map(|line| {
-        line.unwrap().trim_end().parse::<u32>().unwrap()
-    }).collect();
-
+fn part1(nums: &[u32]) {
     let first = nums[0];
 
     let (_, part1): (u32, u32) =
@@ -11,4 +7,31 @@ fn main() {
                                  (next, count + (next > prev) as u32));
 
     println!("part 1: {}", part1);
+}
+
+fn part2(nums: &[u32]) {
+    let (a, b, c) = (nums[0], nums[1], nums[2]);
+
+    fn fold((a, b, c, count): (u32, u32, u32, u32), &next: &u32)
+            -> (u32, u32, u32, u32) {
+        let part_sum = b + c;
+        let prev_sum = a + part_sum;
+        let next_sum = part_sum + next;
+
+        (b, c, next, count + (next_sum > prev_sum) as u32)
+    }
+
+    let (_, _, _, part2): (u32, u32, u32, u32) =
+        nums.iter().skip(3).fold((a, b, c, 0), fold);
+
+    println!("part 2: {}", part2);
+}
+
+fn main() {
+    let nums: Vec<u32> = std::io::stdin().lines().map(|line| {
+        line.unwrap().trim_end().parse::<u32>().unwrap()
+    }).collect();
+
+    part1(&nums);
+    part2(&nums);
 }
