ecafa9422e99d2f44d0a0c497ec1af72ffe02df5
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-03 17:05:06 +0100

    2021/2/1: Separate out the command parsing from the processing

--- 2021/day2/src/main.rs
+++ 2021/day2/src/main.rs
@@ -3,30 +3,53 @@ struct Position {
     depth: i32,
 }
 
+enum CommandKeyword {
+    Forward,
+    Down,
+    Up,
+}
+
+struct Command {
+    keyword: CommandKeyword,
+    amount: i32,
+}
+
+impl std::str::FromStr for Command {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let space = match s.find(' ') {
+            Some(i) => i,
+            None => return Err("No space in command".to_string()),
+        };
+
+        let amount = match s[space + 1..].trim_end().parse::<i32>() {
+            Ok(val) => val,
+            Err(..) => return Err("Invalid integer in command".to_string()),
+        };
+
+        let keyword = match &s[0..space] {
+            "forward" => CommandKeyword::Forward,
+            "down" => CommandKeyword::Down,
+            "up" => CommandKeyword::Up,
+            text => return Err("Unknown command: ".to_string() + text),
+        };
+
+        Ok(Command { keyword, amount })
+    }
+}
+
 impl Position {
     fn new() -> Position {
         Position { horizontal: 0, depth: 0 }
     }
 
-    fn run_command(&mut self, command: &str) -> Result<(), String> {
-        let space = match command.find(' ') {
-            Some(i) => i,
-            None => return Err("No space in command".to_string()),
-        };
-
-        let amount = match command[space + 1..].trim_end().parse::<i32>() {
-            Ok(val) => val,
-            Err(..) => return Err("Invalid integer in command".to_string()),
-        };
-
-        match &command[0..space] {
-            "forward" => self.horizontal += amount,
-            "down" => self.depth += amount,
-            "up" => self.depth -= amount,
-            keyword => return Err("Unknown command: ".to_string() + keyword),
+    fn run_command(&mut self, command: &Command) {
+        match command.keyword {
+            CommandKeyword::Forward => self.horizontal += command.amount,
+            CommandKeyword::Down => self.depth += command.amount,
+            CommandKeyword::Up => self.depth -= command.amount,
         }
-
-        Ok(())
     }
 }
 
@@ -42,10 +65,15 @@ fn main() {
             Ok(line) => line,
         };
 
-        if let Err(message) = pos.run_command(&line) {
-            eprintln!("line {}: {}", line_num + 1, message);
-            std::process::exit(1);
-        }
+        let command = match line.parse::<Command>() {
+            Err(message) => {
+                eprintln!("line {}: {}", line_num + 1, message);
+                std::process::exit(1);
+            },
+            Ok(command) => command,
+        };
+
+        pos.run_command(&command);
     }
 
     println!("Horizontal position = {}, Depth = {}", pos.horizontal, pos.depth);
