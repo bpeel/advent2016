defef0204e7fb559691581f5c3912059bde16f84
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-05 11:29:28 +0100

    2022/5/2

--- 2022/day5/src/main.rs
+++ 2022/day5/src/main.rs
@@ -11,13 +11,19 @@ impl State {
         State { stacks: Vec::new() }
     }
 
-    fn apply_move(&mut self, mov: &Move) -> Result<(), String> {
+    fn check_move_stacks_valid(&self, mov: &Move) -> Result<(), String> {
         if mov.source < 1 ||
             mov.source as usize > self.stacks.len() ||
             mov.dest < 1 ||
             mov.dest as usize > self.stacks.len() {
             return Err("Invalid source or destination in move".to_string());
         }
+
+        Ok(())
+    }
+
+    fn apply_move_part1(&mut self, mov: &Move) -> Result<(), String> {
+        self.check_move_stacks_valid(mov)?;
 
         for _ in 0..mov.amount {
             let c = match self.stacks[mov.source as usize - 1].pop() {
@@ -28,6 +34,41 @@ impl State {
 
             self.stacks[mov.dest as usize - 1].push(c);
         }
+
+        Ok(())
+    }
+
+    fn apply_move_part2(&mut self, mov: &Move) -> Result<(), String> {
+        self.check_move_stacks_valid(mov)?;
+
+        let source = mov.source as usize - 1;
+        let dest = mov.dest as usize - 1;
+
+        if source == dest {
+            // The result will be the same as if we didn’t do anything
+            return Ok(());
+        }
+
+        let source_len = self.stacks[source].len();
+
+        if source_len < mov.amount as usize {
+            return Err(format!("Not enough crates in stack to move {} items",
+                               mov.amount));
+        }
+
+        let dest: *mut Stack = &mut self.stacks[dest];
+
+        // SAFETY: We need to get a mutable reference to the dest
+        // stack while having an immutable reference source stack. I
+        // think this would mean borrowing self.stacks twice and the
+        // borrow checker won’t allow this. We know it’s safe however
+        // because we are modifying two different members.
+        unsafe {
+            (*dest).extend(&self.stacks[source][source_len -
+                                                mov.amount as usize..]);
+        }
+
+        self.stacks[source].truncate(source_len - mov.amount as usize);
 
         Ok(())
     }
@@ -169,7 +210,7 @@ fn read_data<I>(lines: &mut I) -> Result<(State, Vec<Move>), String>
 fn main() {
     let mut lines = std::io::stdin().lines();
 
-    let (mut state, moves) = match read_data(&mut lines) {
+    let (state, moves) = match read_data(&mut lines) {
         Ok(pair) => pair,
 
         Err(e) => {
@@ -178,12 +219,17 @@ fn main() {
         },
     };
 
-    for (move_num, mov) in moves.iter().enumerate() {
-        if let Err(e) = state.apply_move(mov) {
-            eprintln!("move {}: {}", move_num + 1, e);
-            std::process::exit(1);
-        }
-    }
+    for (part_num, func) in [State::apply_move_part1,
+                             State::apply_move_part2].iter().enumerate() {
+        let mut state = state.clone();
 
-    println!("part 1: {}", state.tops());
+        for (move_num, mov) in moves.iter().enumerate() {
+            if let Err(e) = func(&mut state, mov) {
+                eprintln!("move {}: {}", move_num + 1, e);
+                std::process::exit(1);
+            }
+        }
+
+        println!("part {}: {}", part_num + 1, state.tops());
+    }
 }
