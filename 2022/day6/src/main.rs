1dc6b5a5e558458ea3688907a69bb0b77c0006d8
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-06 06:28:02 +0100

    2022/6: Clean up the solution

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -1,16 +1,41 @@
 use std::collections::HashSet;
 
+fn find_marker(data: &str, marker_length: usize) -> Option<usize> {
+    if data.len() < marker_length {
+        return None;
+    }
+
+    let mut set = HashSet::<u8>::new();
+
+    for i in 0..=data.len() - marker_length {
+        set.clear();
+        set.extend(&data.as_bytes()[i..i + marker_length]);
+
+        if set.len() >= marker_length {
+            return Some(i + marker_length);
+        }
+    }
+
+    None
+}
+
 fn main() {
-    let text = std::io::stdin().lines().next().unwrap().unwrap();
+    for result in std::io::stdin().lines() {
+        let line = match result {
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            },
+            Ok(line) => line,
+        };
 
-    for i in 0..text.len() - 14 {
-        let mut hs = HashSet::<u8>::new();
+        for (part_num, &marker_length) in [4, 14].iter().enumerate() {
+            let result = match find_marker(&line, marker_length) {
+                Some(n) => n.to_string(),
+                None => "marker not found".to_string(),
+            };
 
-        hs.extend(&text.as_bytes()[i..i+14]);
-
-        if hs.len() == 14 {
-            println!("part 1 {}", i + 14);
-            break;
+            println!("part {}: {}", part_num + 1, result);
         }
     }
 }
