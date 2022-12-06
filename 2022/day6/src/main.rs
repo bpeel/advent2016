1709a434544bdce9ef82b8f82e83d588e245f8a8
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-06 06:10:30 +0100

    2022/6/2
    I tried to do this as quickly as possible so it’s not good code.

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -3,13 +3,13 @@ use std::collections::HashSet;
 fn main() {
     let text = std::io::stdin().lines().next().unwrap().unwrap();
 
-    for i in 0..text.len() - 4 {
+    for i in 0..text.len() - 14 {
         let mut hs = HashSet::<u8>::new();
 
-        hs.extend(&text.as_bytes()[i..i+4]);
+        hs.extend(&text.as_bytes()[i..i+14]);
 
-        if hs.len() == 4 {
-            println!("part 1 {}", i + 4);
+        if hs.len() == 14 {
+            println!("part 1 {}", i + 14);
             break;
         }
     }
