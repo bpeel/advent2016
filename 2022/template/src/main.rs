c4da20852a84f1c1b0485170d30f8e60da05e26d
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-08 16:40:31 +0100

    Add a template project with some utilities

---
+++ 2022/template/src/main.rs
@@ -0,0 +1,66 @@
+mod util;
+
+use std::io::BufRead;
+
+#[derive(Debug, Clone)]
+struct Item {
+    start: u32,
+    end: u32,
+}
+
+fn read_items<I>(lines: &mut I) -> Result<Vec<Item>, String>
+    where I: Iterator<Item = Result<String, std::io::Error>>
+{
+    let re = regex::Regex::new(r"^(\d+),(\d+)$").unwrap();
+    let mut items = Vec::<Item>::new();
+
+    for (line_num, result) in lines.enumerate() {
+        let line = match result {
+            Err(e) => e.to_string(),
+            Ok(line) => line,
+        };
+
+        let captures = match re.captures(&line) {
+            Some(c) => c,
+            None => return Err(format!("line: {}: invalid syntax",
+                                       line_num + 1)),
+        };
+
+        items.push(Item {
+            start: captures[1].parse::<u32>().unwrap(),
+            end: captures[2].parse::<u32>().unwrap(),
+        });
+    }
+
+    Ok(items)
+}
+
+fn main() -> std::process::ExitCode {
+    let grid;
+    let items;
+
+    {
+        let mut input = std::io::stdin().lock();
+
+        grid = match util::Grid::load(&mut input) {
+            Err(e) => {
+                eprintln!("{}", e);
+                return std::process::ExitCode::FAILURE;
+            },
+            Ok(grid) => grid,
+        };
+
+        items = match read_items(&mut input.lines()) {
+            Err(e) => {
+                eprintln!("{}", e);
+                return std::process::ExitCode::FAILURE;
+            },
+            Ok(items) => items,
+        };
+    }
+
+    println!("{}", grid);
+    println!("{:?}", items);
+
+    std::process::ExitCode::SUCCESS
+}
