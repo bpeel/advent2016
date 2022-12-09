e8902ccea3e7b4304d2cd95c654c64e9092b01df
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-09 07:29:37 +0100

    2022/9: Tidy up the code
    Remove unused code from the template.
    Do both parts.

--- 2022/day9/src/main.rs
+++ 2022/day9/src/main.rs
@@ -1,17 +1,66 @@
 use std::collections::HashSet;
-use std::io::BufRead;
 
 #[derive(Debug, Clone)]
-struct Item {
-    direction: char,
+struct RopeLink {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone)]
+struct Rope {
+    links: Vec<RopeLink>,
+}
+
+impl Rope {
+    fn new(size: usize) -> Rope {
+        Rope {
+            links: std::iter::repeat(RopeLink { x: 0, y: 0 })
+                .take(size)
+                .collect()
+        }
+    }
+
+    fn move_head(&mut self, direction_start: (i32, i32)) {
+        let mut direction = direction_start;
+        let mut head = self.links.len() - 1;
+
+        loop {
+            self.links[head].x += direction.0;
+            self.links[head].y += direction.1;
+
+            if head == 0 {
+                return;
+            }
+
+            let tail = head - 1;
+
+            if (self.links[head].x - self.links[tail].x).abs() <= 1 &&
+                (self.links[head].y - self.links[tail].y).abs() <= 1 {
+                break;
+            }
+
+            direction = ((self.links[head].x - self.links[tail].x).signum(),
+                         (self.links[head].y - self.links[tail].y).signum());
+            head = tail;
+        }
+    }
+
+    fn tail(&self) -> (i32, i32) {
+        (self.links[0].x, self.links[0].y)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Command {
+    direction: (i32, i32),
     count: usize,
 }
 
-fn read_items<I>(lines: &mut I) -> Result<Vec<Item>, String>
+fn read_commands<I>(lines: &mut I) -> Result<Vec<Command>, String>
     where I: Iterator<Item = Result<String, std::io::Error>>
 {
     let re = regex::Regex::new(r"^([UDLR]) (\d+)$").unwrap();
-    let mut items = Vec::<Item>::new();
+    let mut commands = Vec::<Command>::new();
 
     for (line_num, result) in lines.enumerate() {
         let line = match result {
@@ -25,88 +74,54 @@ fn read_items<I>(lines: &mut I) -> Result<Vec<Item>, String>
                                        line_num + 1)),
         };
 
-        items.push(Item {
-            direction: (&captures[1]).chars().nth(0).unwrap(),
-            count: captures[2].parse::<usize>().unwrap(),
-        });
-    }
-
-    Ok(items)
-}
-
-#[derive(Eq, Hash, PartialEq, Clone, Debug)]
-struct Pos {
-    x: i32,
-    y: i32,
-}
-
-fn move_rope(rope: &mut [Pos], direction: (i32, i32)) {
-    let head = rope.len() - 1;
-
-    rope[head].x += direction.0;
-    rope[head].y += direction.1;
-
-    if head == 0 {
-        return;
-    }
-
-    let tail = head - 1;
-
-    let horiz = (rope[head].x - rope[tail].x).abs() > 1;
-    let vert = (rope[head].y - rope[tail].y).abs() > 1;
-
-    if horiz || vert {
-        let dir = ((rope[head].x - rope[tail].x).signum(),
-                   (rope[head].y - rope[tail].y).signum());
-        move_rope(&mut rope[0..head], dir);
-    }
-}
-
-fn main() -> std::process::ExitCode {
-    let items;
-
-    {
-        let input = std::io::stdin().lock();
-
-        items = match read_items(&mut input.lines()) {
-            Err(e) => {
-                eprintln!("{}", e);
-                return std::process::ExitCode::FAILURE;
-            },
-            Ok(items) => items,
+        let direction = match &captures[1] {
+            "U" => (0, -1),
+            "D" => (0, 1),
+            "L" => (-1, 0),
+            "R" => (1, 0),
+            _ => panic!("regex returned impossible char"),
         };
+
+        let count = match captures[2].parse::<usize>() {
+            Ok(n) => n,
+            Err(e) => return Err(format!("line {}: {}",
+                                         line_num + 1,
+                                         e)),
+        };
+
+        commands.push(Command { direction, count });
     }
 
-    let mut visited = HashSet::<Pos>::new();
-    let rope_start = Pos { x: 0, y: 0 };
-    let mut rope: Vec<Pos> = std::iter::repeat(rope_start).take(10).collect();
+    Ok(commands)
+}
 
-    visited.insert(rope[0].clone());
+fn run_part(commands: &[Command], rope_length: usize) -> usize {
+    let mut visited = HashSet::<(i32, i32)>::new();
+    let mut rope = Rope::new(rope_length);
 
-    for item in items {
-        for _ in 0..item.count {
-            let direction = match item.direction {
-                'U' => {
-                    (0, -1)
-                },
-                'D' => {
-                    (0, 1)
-                },
-                'L' => {
-                    (-1, 0)
-                },
-                'R' => {
-                    (1, 0)
-                },
-                _ => panic!("??"),
-            };
-            move_rope(&mut rope, direction);
-            println!("{:?} {:?} {:?}", item.direction, rope[9], rope[0]);
-            visited.insert(rope[0].clone());
+    visited.insert(rope.tail());
+
+    for command in commands {
+        for _ in 0..command.count {
+            rope.move_head(command.direction);
+            visited.insert(rope.tail());
         }
     }
 
-    println!("{:?}", visited.len());
+    visited.len()
+}
+
+fn main() -> std::process::ExitCode {
+    let commands = match read_commands(&mut std::io::stdin().lines()) {
+        Err(e) => {
+            eprintln!("{}", e);
+            return std::process::ExitCode::FAILURE;
+        },
+        Ok(commands) => commands,
+    };
+
+    println!("part 1: {}", run_part(&commands, 2));
+    println!("part 2: {}", run_part(&commands, 10));
 
     std::process::ExitCode::SUCCESS
 }
