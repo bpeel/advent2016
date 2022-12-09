19adf7c4a68697266364910d927d9af6e8e3619c
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-09 17:54:15 +0100

    template: Add a bunch of helper methods for walker::Direction

--- 2022/template/src/walker.rs
+++ 2022/template/src/walker.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Direction {
     UP,
     DOWN,
@@ -19,21 +19,55 @@ impl Direction {
         }
     }
 
-    fn move_pos(self, (x, y): (i32, i32)) -> (i32, i32) {
+    pub fn opposite(self) -> Direction {
         match self {
-            Direction::UP => (x, y - 1),
-            Direction::DOWN => (x, y + 1),
-            Direction::LEFT => (x - 1, y),
-            Direction::RIGHT => (x + 1, y),
+            Direction::UP => Direction::DOWN,
+            Direction::DOWN => Direction::UP,
+            Direction::LEFT => Direction::RIGHT,
+            Direction::RIGHT => Direction::LEFT,
         }
     }
 
-    fn revert_pos(self, (x, y): (i32, i32)) -> (i32, i32) {
+    pub fn offset(self) -> (i32, i32) {
         match self {
-            Direction::UP => (x, y + 1),
-            Direction::DOWN => (x, y - 1),
-            Direction::LEFT => (x + 1, y),
-            Direction::RIGHT => (x - 1, y),
+            Direction::UP => (0, -1),
+            Direction::DOWN => (0, 1),
+            Direction::LEFT => (-1, 0),
+            Direction::RIGHT => (1, 0),
+        }
+    }
+
+    pub fn move_pos(self, (x, y): (i32, i32)) -> (i32, i32) {
+        let (dx, dy) = self.offset();
+        (x + dx, y + dy)
+    }
+
+    pub fn revert_pos(self, (x, y): (i32, i32)) -> (i32, i32) {
+        let (dx, dy) = self.offset();
+        (x - dx, y - dy)
+    }
+
+    pub fn from_char(ch: char) -> Option<Direction> {
+        match ch.to_ascii_lowercase() {
+            'u' | 'n' => Some(Direction::UP),
+            'd' | 's' => Some(Direction::DOWN),
+            'l' | 'w' => Some(Direction::LEFT),
+            'r' | 'e' => Some(Direction::RIGHT),
+            _ => None
+        }
+    }
+}
+
+impl std::str::FromStr for Direction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &s.to_ascii_lowercase()[..] {
+            "u" | "n" | "up" | "north" => Ok(Direction::UP),
+            "d" | "s" | "down" | "south" => Ok(Direction::DOWN),
+            "l" | "w" | "left" | "west" => Ok(Direction::LEFT),
+            "r" | "e" | "right" | "east" => Ok(Direction::RIGHT),
+            _ => Err(format!("unknown direction: {}", s)),
         }
     }
 }
@@ -101,4 +135,53 @@ pub fn shortest_walk<F>(start_pos: (i32, i32), mut visit_func: F)
             },
         }
     });
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_direction() {
+        fn check_direction_string(tests: &[&str], direction: Direction) {
+            for s in tests {
+                match s.parse::<Direction>() {
+                    Ok(d) => assert_eq!(d, direction),
+                    Err(..) => panic!("failed to parse {} into direction", s),
+                }
+
+                let first_ch = s.chars().next().unwrap();
+
+                match Direction::from_char(first_ch) {
+                    Some(d) => assert_eq!(d, direction),
+                    None => panic!("failed to parse {} into direction",
+                                   first_ch),
+                }
+            }
+        }
+        check_direction_string(&["up", "u", "UP", "U", "Up", "NoRTH", "n"],
+                               Direction::UP);
+        check_direction_string(&["down", "d", "DOWN", "D", "Down",
+                                 "SoUTH", "s"],
+                               Direction::DOWN);
+        check_direction_string(&["left", "l", "LEFT", "L", "Left",
+                                 "West", "w"],
+                               Direction::LEFT);
+        check_direction_string(&["right", "r", "RIGHT", "R", "Right",
+                                 "East", "E"],
+                               Direction::RIGHT);
+
+        for d in "udlr".chars() {
+            let d = Direction::from_char(d).unwrap();
+            let offset = d.offset();
+            let opposite = d.opposite().offset();
+            assert_eq!(offset.0, -opposite.0);
+            assert_eq!(offset.1, -opposite.1);
+        }
+
+        assert_eq!(Direction::UP.offset(), (0, -1));
+        assert_eq!(Direction::DOWN.offset(), (0, 1));
+        assert_eq!(Direction::LEFT.offset(), (-1, 0));
+        assert_eq!(Direction::RIGHT.offset(), (1, 0));
+    }
 }
