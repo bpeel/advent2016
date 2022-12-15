c4224f59447ba43929d0920003ea17e54cc881e9
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-15 17:16:36 +0100

    2021/9: Attempt at part 2
    This gets the right answer for the example but not for the real input :/

--- 2021/day9/src/main.rs
+++ 2021/day9/src/main.rs
@@ -1,6 +1,9 @@
 mod util;
+mod walker;
 
 use util::Grid;
+use walker::VisitResult;
+use std::collections::HashSet;
 
 fn is_low_point(grid: &Grid, pos: (i32, i32)) -> bool {
     let this_height = grid.get(pos).unwrap();
@@ -47,6 +50,51 @@ fn part1(grid: &Grid) -> usize {
     sum
 }
 
+fn part2(grid: &Grid) -> String {
+    let mut basin_sizes = Vec::<usize>::new();
+
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let pos = (x as i32, y as i32);
+
+            if !is_low_point(grid, pos) {
+                continue;
+            }
+
+            let mut visited_locations = HashSet::<(i32, i32)>::new();
+
+            walker::walk::<walker::QuadDirection, _>(pos, |path, pos| {
+                let can_visit = match grid.get(pos) {
+                    None => false,
+                    Some(this_height) if this_height >= b'9' => false,
+                    Some(this_height) => match path.last() {
+                        Some(&(_, last_pos)) =>
+                            this_height == grid.get(last_pos).unwrap() + 1,
+                        None => true,
+                    },
+                };
+
+                if can_visit && !visited_locations.contains(&pos) {
+                    visited_locations.insert(pos);
+                    VisitResult::Continue
+                } else {
+                    VisitResult::Backtrack
+                }
+            });
+
+            basin_sizes.push(visited_locations.len());
+        }
+    }
+
+    if basin_sizes.len() < 3 {
+        return format!("only {} basins found", basin_sizes.len());
+    }
+
+    basin_sizes.sort_by(|a, b| b.cmp(a));
+
+    format!("{}", basin_sizes.iter().take(3).fold(1, |a, &b| a * b))
+}
+
 fn main() -> std::process::ExitCode {
     let grid = match Grid::load(&mut std::io::stdin().lock()) {
         Err(e) => {
@@ -57,6 +105,7 @@ fn main() -> std::process::ExitCode {
     };
 
     println!("part 1: {}", part1(&grid));
+    println!("part 2: {}", part2(&grid));
 
     std::process::ExitCode::SUCCESS
 }
---
+++ 2021/day9/src/walker.rs
@@ -0,0 +1,88 @@
+pub trait Direction: Sized + Clone + Copy {
+    type Pos: Clone + Copy;
+
+    fn first_direction() -> Self;
+    fn next_direction(self) -> Option<Self>;
+    fn move_pos(self, pos: Self::Pos) -> Self::Pos;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum QuadDirection {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction for QuadDirection {
+    type Pos = (i32, i32);
+
+    fn first_direction() -> QuadDirection {
+        QuadDirection::Up
+    }
+
+    fn next_direction(self) -> Option<QuadDirection> {
+        match self {
+            QuadDirection::Up => Some(QuadDirection::Down),
+            QuadDirection::Down => Some(QuadDirection::Left),
+            QuadDirection::Left => Some(QuadDirection::Right),
+            QuadDirection::Right => None,
+        }
+    }
+
+    fn move_pos(self, (x, y): (i32, i32)) -> (i32, i32) {
+        let (dx, dy) = self.offset();
+        (x + dx, y + dy)
+    }
+}
+
+impl QuadDirection {
+    pub fn offset(self) -> (i32, i32) {
+        match self {
+            QuadDirection::Up => (0, -1),
+            QuadDirection::Down => (0, 1),
+            QuadDirection::Left => (-1, 0),
+            QuadDirection::Right => (1, 0),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum VisitResult {
+    Continue,
+    Backtrack,
+}
+
+pub fn walk<D, F>(start_pos: D::Pos, mut visit_func: F)
+    where F: FnMut(&[(D, D::Pos)], D::Pos) -> VisitResult,
+          D: Direction
+{
+    let mut stack = Vec::<(D, D::Pos)>::new();
+    let mut pos = start_pos;
+
+    loop {
+        match visit_func(&stack, pos) {
+            VisitResult::Continue => {
+                let first_direction = D::first_direction();
+                stack.push((D::first_direction(), pos));
+                pos = first_direction.move_pos(pos);
+            },
+            VisitResult::Backtrack => {
+                loop {
+                    let (last_direction, last_pos) = match stack.pop() {
+                        Some(s) => s,
+                        None => return,
+                    };
+
+                    pos = last_pos;
+
+                    if let Some(d) = last_direction.next_direction() {
+                        stack.push((d, last_pos));
+                        pos = d.move_pos(pos);
+                        break;
+                    }
+                }
+            },
+        }
+    }
+}
