c4c3a931974859fc802aa3acc226f97c6125de69
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-12-16 18:16:29 +0100

    2022/day16: Store the valves in a compact array instead of HashMap

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -2,8 +2,15 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 struct Valve {
+    name: String,
     flow_rate: u8,
-    tunnels: Vec<u16>,
+    tunnels: Vec<u8>,
+}
+
+struct LoadingValve {
+    name: String,
+    flow_rate: u8,
+    tunnels: Vec<String>,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -15,21 +22,21 @@ enum Action {
 
 #[derive(Debug)]
 struct Walker<'a, const N_ACTORS: usize> {
-    stack: Vec<([Action; N_ACTORS], [u16; N_ACTORS])>,
-    pos: [u16; N_ACTORS],
-    open_valves: HashMap<u16, u8>,
-    valves: &'a HashMap<u16, Valve>,
+    stack: Vec<([Action; N_ACTORS], [u8; N_ACTORS])>,
+    pos: [u8; N_ACTORS],
+    open_valves: HashMap<u8, u8>,
+    valves: &'a Vec<Valve>,
     best_score: usize,
 }
 
 impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
     const START_TIME: usize = (N_ACTORS - 1) * 4;
 
-    fn new(valves: &'a HashMap<u16, Valve>) -> Walker<'a, N_ACTORS> {
+    fn new(valves: &'a Vec<Valve>) -> Walker<'a, N_ACTORS> {
         Walker {
-            stack: Vec::<([Action; N_ACTORS], [u16; N_ACTORS])>::new(),
-            pos: [valve_num(&"AA"); N_ACTORS],
-            open_valves: HashMap::<u16, u8>::new(),
+            stack: Vec::<([Action; N_ACTORS], [u8; N_ACTORS])>::new(),
+            pos: [0; N_ACTORS],
+            open_valves: HashMap::<u8, u8>::new(),
             valves,
             best_score: usize::MIN,
         }
@@ -39,7 +46,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
         let score = self.open_valves
             .iter()
             .map(|(valve, &open_time)|
-                 self.valves[valve].flow_rate as usize *
+                 self.valves[*valve as usize].flow_rate as usize *
                  (TOTAL_TIME - Self::START_TIME - open_time as usize))
             .sum::<usize>();
 
@@ -63,9 +70,9 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
                         Action::OpenValve => print!("op"),
                         Action::TakeTunnel(tunnel_num) => {
                             let tunnel_num = tunnel_num as usize;
-                            let valve = &self.valves[&last_pos[actor]];
+                            let valve = &self.valves[last_pos[actor] as usize];
                             let next_valve = valve.tunnels[tunnel_num];
-                            print!("{}", valve_name(next_valve));
+                            print!("{}", self.valves[next_valve as usize].name);
                         },
                         Action::StayStill => print!("**"),
                     }
@@ -78,7 +85,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
         }
     }
 
-    fn have_visited_since_last_open(&self, valve: u16) -> bool {
+    fn have_visited_since_last_open(&self, valve: u8) -> bool {
         for (action, pos) in self.stack.iter().rev() {
             for action in action.iter() {
                 if let Action::OpenValve = action {
@@ -99,7 +106,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
     fn can_take_tunnel(&self,
                        actor: usize,
                        tunnel_num: usize) -> bool {
-        let valve = &self.valves[&self.pos[actor]];
+        let valve = &self.valves[self.pos[actor] as usize];
 
         if tunnel_num >= valve.tunnels.len() {
             return false;
@@ -121,7 +128,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
             return false;
         }
 
-        let valve = &self.valves[&self.pos[actor]];
+        let valve = &self.valves[self.pos[actor] as usize];
 
         // Don’t bother opening valves that have zero flow rate
         if valve.flow_rate == 0 {
@@ -137,7 +144,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
                 continue;
             }
 
-            let other_valve = &self.valves[&pos[actor]];
+            let other_valve = &self.valves[pos[actor] as usize];
 
             if other_valve.flow_rate > valve.flow_rate {
                 return false;
@@ -153,7 +160,8 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
                 Action::StayStill => {
                     // There’s no point in allowing an actor to stay
                     // still if there’s a tunnel they can go to
-                    let n_tunnels = self.valves[&self.pos[actor]].tunnels.len();
+                    let valve = &self.valves[self.pos[actor] as usize];
+                    let n_tunnels = valve.tunnels.len();
                     for i in 0..n_tunnels {
                         if self.can_take_tunnel(actor, i) {
                             return false;
@@ -204,7 +212,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
                             break 'find_action;
                         },
                         Action::TakeTunnel(t) => {
-                            let valve = &self.valves[&self.pos[i]];
+                            let valve = &self.valves[self.pos[i] as usize];
 
                             if t as usize + 1 >= valve.tunnels.len() {
                                 action[i] = Action::StayStill;
@@ -237,7 +245,7 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
                                             self.stack.len() as u8);
                 },
                 Action::TakeTunnel(t) => {
-                    let valve = &self.valves[&self.pos[actor]];
+                    let valve = &self.valves[self.pos[actor] as usize];
                     self.pos[actor] = valve.tunnels[*t as usize];
                 },
                 Action::StayStill => (),
@@ -298,26 +306,14 @@ impl<'a, const N_ACTORS: usize> Walker<'a, N_ACTORS> {
 
 const TOTAL_TIME: usize = 30;
 
-fn valve_num(name: &str) -> u16 {
-    // The valve name should be validated by the regexp so we can
-    // just use unwrap here
-    (((name.as_bytes()[0] - b'A') as u16) << 8) |
-    (name.as_bytes()[1] - b'A') as u16
-}
-
-fn valve_name(num: u16) -> String {
-    format!("{}{}",
-            ((num >> 8) as u8 + b'A') as char,
-            ((num & 0xff) as u8 + b'A') as char)
-}
-
-fn read_valves<I>(lines: &mut I) -> Result<HashMap<u16, Valve>, String>
+fn read_valves<I>(lines: &mut I) -> Result<Vec<Valve>, String>
     where I: Iterator<Item = Result<String, std::io::Error>>
 {
     let re = regex::Regex::new("^Valve ([A-Z]{2}) has flow rate=(\\d+); \
                                 tunnels? leads? to valves? ((?:[A-Z]{2}, )*\
                                 [A-Z]{2})$").unwrap();
-    let mut valves = HashMap::<u16, Valve>::new();
+    let mut valves = Vec::<LoadingValve>::new();
+    let mut valve_indices = HashMap::<String, usize>::new();
 
     for (line_num, result) in lines.enumerate() {
         let line = match result {
@@ -336,36 +332,64 @@ fn read_valves<I>(lines: &mut I) -> Result<HashMap<u16, Valve>, String>
             Ok(r) => r,
         };
 
-        let this_valve_num = valve_num(&captures[1]);
+        let valve_name = captures[1].to_string();
 
-        if let Some(_) = valves.get(&this_valve_num) {
-            return Err(format!("line {}: duplicate tunnel", line_num + 1));
+        if let Some(_) = valve_indices.get(&valve_name) {
+            return Err(format!("line {}: duplicate valve", line_num + 1));
         }
 
-        valves.insert(this_valve_num, Valve {
+        valve_indices.insert(valve_name.clone(), valves.len());
+
+        valves.push(LoadingValve {
+            name: valve_name,
             flow_rate,
             tunnels: captures[3]
                 .split(", ")
-                .map(|s| valve_num(s))
+                .map(|s| s.to_string())
                 .collect(),
         });
     }
 
-    for (&valve_num, valve) in valves.iter() {
-        for &target_valve in valve.tunnels.iter() {
-            if let None = valves.get(&target_valve) {
-                return Err(format!("valve {} links to non-existant valve {}",
-                                   valve_name(valve_num),
-                                   valve_name(target_valve)));
-            }
+    if valves.len() > u64::BITS as usize {
+        return Err("too many valves".to_string());
+    }
+
+    // Make sure that AA is valve 0
+    match valve_indices.get("AA") {
+        None => return Err("missing valve AA".to_string()),
+        Some(0) => (),
+        Some(&index) => {
+            let other_name = &valves[0].name;
+            valve_indices.insert(other_name.to_string(), index);
+            valve_indices.insert("AA".to_string(), 0);
+            let (left, right) = valves.split_at_mut(index);
+            std::mem::swap(&mut left[0], &mut right[0]);
         }
     }
 
-    if let None = valves.get(&valve_num(&"AA")) {
-        return Err("missing valve AA".to_string());
+    let mut final_valves = Vec::<Valve>::new();
+
+    for valve in valves.iter() {
+        let mut final_valve = Valve {
+            name: valve.name.clone(),
+            flow_rate: valve.flow_rate,
+            tunnels: Vec::<u8>::new(),
+        };
+
+        for target_valve in valve.tunnels.iter() {
+            match valve_indices.get(target_valve) {
+                Some(&index) => final_valve.tunnels.push(index as u8),
+                None => return Err(format!("valve {} links to non-existant \
+                                            valve {}",
+                                           valve.name,
+                                           target_valve)),
+            }
+        }
+
+        final_valves.push(final_valve);
     }
 
-    Ok(valves)
+    Ok(final_valves)
 }
 
 fn main() -> std::process::ExitCode {
