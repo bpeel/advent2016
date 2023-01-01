63f36c7957b1bf633fc7687b1b9afe881c45885a
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-01-01 02:07:50 +0100

    2022/21: part 2

--- 2022/day21/src/main.rs
+++ 2022/day21/src/main.rs
@@ -34,6 +34,7 @@ enum Expression {
         op: Operator,
         b: Box<Expression>,
     },
+    Marker,
 }
 
 enum BuildStackEntry {
@@ -124,47 +125,55 @@ fn read_monkies<I>(lines: &mut I) ->
 }
 
 fn build_expression(monkies: &HashMap<u32, NamedExpression>,
-                    monkey: u32) ->
+                    monkey: u32,
+                    marker: Option<u32>) ->
     Result<Expression, String>
 {
     let mut stack = Vec::<BuildStackEntry>::new();
     let mut next_monkey = monkey;
 
     loop {
-        let named_expression = match monkies.get(&next_monkey) {
-            None => return Err(format!("missing monkey {}",
-                                       int_to_monkey_name(next_monkey))),
-            Some(e) => e,
-        };
+        let mut expression = match marker {
+            Some(m) if m == next_monkey => Expression::Marker,
+            _ => {
+                let named_expression = match monkies.get(&next_monkey) {
+                    None => {
+                        return Err(format!("missing monkey {}",
+                                           int_to_monkey_name(next_monkey)));
+                    },
+                    Some(e) => e,
+                };
 
-        match named_expression {
-            NamedExpression::Constant(c) => {
-                let mut expression = Expression::Constant(*c);
-
-                loop {
-                    match stack.pop() {
-                        None => return Ok(expression),
-                        Some(BuildStackEntry::NeedLeft(op, right)) => {
-                            let left = Box::new(expression);
-                            stack.push(BuildStackEntry::NeedRight(left, op));
-                            next_monkey = right;
-                            break;
-                        },
-                        Some(BuildStackEntry::NeedRight(left, op)) => {
-                            expression =
-                                Expression::Operation {
-                                    a: left,
-                                    op,
-                                    b: Box::new(expression),
-                                };
-                        },
-                    };
+                match named_expression {
+                    NamedExpression::Constant(c) =>
+                        Expression::Constant(*c),
+                    &NamedExpression::Operation { a: left, op, b: right } => {
+                        stack.push(BuildStackEntry::NeedLeft(op, right));
+                        next_monkey = left;
+                        continue;
+                    },
                 }
             },
-            &NamedExpression::Operation { a: left, op, b: right } => {
-                stack.push(BuildStackEntry::NeedLeft(op, right));
-                next_monkey = left;
-            },
+        };
+
+        loop {
+            match stack.pop() {
+                None => return Ok(expression),
+                Some(BuildStackEntry::NeedLeft(op, right)) => {
+                    let left = Box::new(expression);
+                    stack.push(BuildStackEntry::NeedRight(left, op));
+                    next_monkey = right;
+                    break;
+                },
+                Some(BuildStackEntry::NeedRight(left, op)) => {
+                    expression =
+                        Expression::Operation {
+                            a: left,
+                            op,
+                            b: Box::new(expression),
+                        };
+                },
+            };
         }
     }
 }
@@ -174,45 +183,136 @@ fn fold_constants(expression: &Expression) -> Expression {
     let mut next_expression = expression;
 
     loop {
-        match next_expression {
-            Expression::Constant(c) => {
-                let mut expression = Expression::Constant(*c);
-
-                loop {
-                    match stack.pop() {
-                        None => return expression,
-                        Some(FoldStackEntry::NeedLeft(op, right)) => {
-                            let entry =
-                                FoldStackEntry::NeedRight(expression, op);
-                            stack.push(entry);
-                            next_expression = right;
-                            break;
-                        },
-                        Some(FoldStackEntry::NeedRight(left, op)) => {
-                            if let Expression::Constant(a) = left {
-                                if let Expression::Constant(b) = expression {
-                                    let r = op.apply(a, b);
-                                    expression = Expression::Constant(r);
-                                    continue;
-                                }
-                            }
-
-                            expression =
-                                Expression::Operation {
-                                    a: Box::new(left),
-                                    op,
-                                    b: Box::new(expression),
-                                };
-                        },
-                    };
-                }
-            },
+        let mut expression = match next_expression {
+            Expression::Constant(c) =>
+                Expression::Constant(*c),
+            Expression::Marker =>
+                Expression::Marker,
             &Expression::Operation { a: ref left, op, b: ref right } => {
                 stack.push(FoldStackEntry::NeedLeft(op, right));
                 next_expression = left;
+                continue;
+            },
+        };
+
+        loop {
+            match stack.pop() {
+                None => return expression,
+                Some(FoldStackEntry::NeedLeft(op, right)) => {
+                    let entry =
+                        FoldStackEntry::NeedRight(expression, op);
+                    stack.push(entry);
+                    next_expression = right;
+                    break;
+                },
+                Some(FoldStackEntry::NeedRight(left, op)) => {
+                    if let Expression::Constant(a) = left {
+                        if let Expression::Constant(b) = expression {
+                            let r = op.apply(a, b);
+                            expression = Expression::Constant(r);
+                            continue;
+                        }
+                    }
+
+                    expression =
+                        Expression::Operation {
+                            a: Box::new(left),
+                            op,
+                            b: Box::new(expression),
+                        };
+                },
+            };
+        }
+    }
+}
+
+fn equate_expression(mut expression: &Expression, mut value: i64) ->
+    Result<i64, String>
+{
+    loop {
+        match expression {
+            Expression::Constant(_) => {
+                break Err("tried to equate value with constant \
+                           expression".to_string());
+            },
+            Expression::Marker => {
+                break Ok(value);
+            },
+            &Expression::Operation { ref a, op, ref b } => {
+                let is_right;
+                let constant;
+
+                if let &Expression::Constant(c) = a.as_ref() {
+                    expression = &b;
+                    constant = c;
+                    is_right = false;
+                } else if let &Expression::Constant(c) = b.as_ref() {
+                    expression = &a;
+                    constant = c;
+                    is_right = true;
+                } else {
+                    return Err("neither side of folded expression is a \
+                                constant".to_string());
+                };
+
+                value = match op {
+                    Operator::Plus => value - constant,
+                    Operator::Minus => {
+                        if is_right {
+                            value + constant
+                        } else {
+                            constant - value
+                        }
+                    },
+                    Operator::Divide => {
+                        if is_right {
+                            value * constant
+                        } else {
+                            constant / value
+                        }
+                    },
+                    Operator::Multiply => value / constant,
+                }
             },
         }
     }
+}
+
+fn part1(monkies: &HashMap<u32, NamedExpression>) -> Result<i64, String> {
+    let expression = build_expression(&monkies,
+                                      monkey_name_to_int("root"),
+                                      None)?;
+
+    match fold_constants(&expression) {
+        Expression::Constant(c) => Ok(c),
+        _ => panic!("constant folding didn’t fold to a constant"),
+    }
+}
+
+fn part2(monkies: &HashMap<u32, NamedExpression>) -> Result<i64, String> {
+    let expression = build_expression(&monkies,
+                                      monkey_name_to_int("root"),
+                                      Some(monkey_name_to_int("humn")))?;
+
+    let (a, b) = match fold_constants(&expression) {
+        Expression::Constant(_) =>
+            return Err("root is a constant".to_string()),
+        Expression::Marker =>
+            return Err("root is a marker".to_string()),
+        Expression::Operation { a, b, .. } =>
+            (a, b),
+    };
+
+    let (expression, constant) = if let Expression::Constant(c) = *a {
+        (b, c)
+    } else if let Expression::Constant(c) = *b {
+        (a, c)
+    } else {
+        return Err("neither side of folded root expression is a \
+                    constant".to_string());
+    };
+
+    equate_expression(&expression, constant)
 }
 
 fn main() -> std::process::ExitCode {
@@ -224,23 +324,27 @@ fn main() -> std::process::ExitCode {
         Ok(m) => m,
     };
 
-    let expression = match build_expression(&monkies,
-                                            monkey_name_to_int("root")) {
+    let mut ret = std::process::ExitCode::SUCCESS;
+
+    print!("part 1: ");
+
+    match part1(&monkies) {
         Err(e) => {
             println!("{}", e);
-            return std::process::ExitCode::FAILURE;
+            ret = std::process::ExitCode::FAILURE;
         },
-        Ok(e) => e,
-    };
-
-    match fold_constants(&expression) {
-        Expression::Constant(c) => {
-            println!("part 1: {}", c);
-        },
-        Expression::Operation { .. } => {
-            panic!("constant folding didn’t fold to a constant");
-        },
+        Ok(v) => println!("{}", v),
     }
 
-    std::process::ExitCode::SUCCESS
+    print!("part 2: ");
+
+    match part2(&monkies) {
+        Err(e) => {
+            println!("{}", e);
+            ret = std::process::ExitCode::FAILURE;
+        },
+        Ok(v) => println!("{}", v),
+    }
+
+    ret
 }
