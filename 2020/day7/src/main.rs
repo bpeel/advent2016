dd73970e6fa08ae14eb84ca238e3135743fdf217
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-06-12 11:57:08 +0200

    2020/7: Finish the parsing code

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -1,7 +1,26 @@
+use std::process::ExitCode;
+use std::fmt;
+
 enum RuleError {
     BadNumber,
     MissingContains,
     MissingNameTerminator,
+    MissingFullStop,
+}
+
+impl fmt::Display for RuleError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                RuleError::BadNumber => "bad number",
+                RuleError::MissingContains => "missing “contains” keyword",
+                RuleError::MissingNameTerminator => "missing “bag” or “bags”",
+                RuleError::MissingFullStop => "missing full stop at end",
+            },
+        )
+    }
 }
 
 struct Bag {
@@ -19,6 +38,10 @@ struct BagSet {
 }
 
 impl BagSet {
+    fn new() -> BagSet {
+        BagSet { bags: Vec::new() }
+    }
+
     fn get_bag(&mut self, name: &str) -> usize {
         for (bag_num, bag) in self.bags.iter().enumerate() {
             if bag.name == name {
@@ -32,38 +55,44 @@ impl BagSet {
     }
 
     fn parse_rule(&mut self, rule: &str) -> Result<(), RuleError> {
-        let name_end = match rule.find(" bags contain ") {
-            Some(n) => n,
-            None => {
-                return Err(RuleError::MissingContains);
-            }
+        let Some(rule) = rule.strip_suffix('.')
+        else {
+            return Err(RuleError::MissingFullStop);
         };
 
-        let bag = self.get_bag(&rule[0..name_end]);
+        let Some((name, tail)) = rule.split_once(" bags contain ")
+        else {
+            return Err(RuleError::MissingContains);
+        };
 
-        for name in rule[name_end..].split(",") {
-            let num_end = match rule.find(" ") {
-                Some(n) => n,
-                None => {
-                    return Err(RuleError::BadNumber);
-                }
+        let bag = self.get_bag(name);
+
+        if tail == "no other bags" {
+            return Ok(());
+        }
+
+        for sub_bag in tail.split(", ") {
+            let Some((num, tail)) = sub_bag.split_once(" ")
+            else {
+                return Err(RuleError::BadNumber);
             };
 
-            let amount: u32 = match rule[0..name_end].parse() {
-                Ok(n) => n,
-                Err(_) => {
-                    return Err(RuleError::BadNumber);
-                }
+            let Ok(amount) = num.parse::<u32>()
+            else {
+                return Err(RuleError::BadNumber);
             };
 
-            let name_end = match name.find(" bag") {
-                Some(n) => n,
-                None => {
-                    return Err(RuleError::MissingNameTerminator)
-                }
+            let name = match tail.strip_suffix(" bags") {
+                Some(name) => name,
+                None => match tail.strip_suffix(" bag") {
+                    Some(name) => name,
+                    None => {
+                        return Err(RuleError::MissingNameTerminator)
+                    },
+                },
             };
 
-            let sub_bag = self.get_bag(&name[num_end..name_end]);
+            let sub_bag = self.get_bag(name);
 
             self.bags[bag].contains.push(BagSpace { amount, bag: sub_bag });
         }
@@ -72,6 +101,31 @@ impl BagSet {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn read_bag_set() -> Result<BagSet, String> {
+    let mut bag_set = BagSet::new();
+
+    for (line_num, line) in std::io::stdin().lines().enumerate() {
+        let line = match line {
+            Ok(line) => line,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        if let Err(e) = bag_set.parse_rule(&line) {
+            return Err(format!("line {}: {}", line_num + 1, e));
+        }
+    }
+
+    Ok(bag_set)
+}
+
+fn main() -> ExitCode {
+    let bag_set = match read_bag_set() {
+        Ok(bag_set) => bag_set,
+        Err(e) => {
+            eprintln!("{}", e);
+            return ExitCode::FAILURE;
+        },
+    };
+
+    ExitCode::SUCCESS
 }
