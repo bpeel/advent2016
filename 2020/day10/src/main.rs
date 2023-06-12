569457f61f1b83a2f1d4a48bc3b857ace425d420
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-06-12 23:08:56 +0200

    2020/10: DAG idea
    This probably ends up the same as the recursive approach

--- 2020/day10/src/main.rs
+++ 2020/day10/src/main.rs
@@ -1,20 +1,73 @@
-fn count_combinations(base_jolts: u32, nums: &[u32]) -> u64 {
-    let Some((&first, tail)) = nums.split_first()
-    else {
-        return 0;
-    };
+fn build_tree(nums: &[u32]) -> Vec<Vec<usize>> {
+    let mut nodes = vec![Vec::new(); nums.len()];
 
-    let Some(&next) = tail.first()
-    else {
-        return 1;
-    };
-
-    if next - base_jolts > 3 {
-        count_combinations(first, tail)
-    } else {
-        count_combinations(base_jolts, tail)
-            + count_combinations(first, tail)
+    // Add each adaptor as a child of the adaptors that it can be connected to
+    for (index, &num) in nums.iter().enumerate() {
+        for parent in index + 1..nums.len() {
+            if nums[parent] - num <= 3 {
+                nodes[parent].push(index);
+            } else {
+                break;
+            }
+        }
     }
+
+    // Add special children for the plug socket
+    for (index, &num) in nums.iter().enumerate() {
+        if num <= 3 {
+            nodes[index].push(usize::MAX);
+        } else {
+            break;
+        }
+    }
+
+    nodes
+}
+
+struct StackEntry<'a> {
+    count: u64,
+    children: std::slice::Iter<'a, usize>,
+}
+
+fn count_paths(nodes: &Vec<Vec<usize>>) -> u64 {
+    let mut stack = vec![StackEntry {
+        count: 0,
+        children: match nodes.last() {
+            Some(last) => last.iter(),
+            None => return 0,
+        },
+    }];
+
+    let mut count = 0u64;
+
+    while let Some(mut entry) = stack.pop() {
+        match entry.children.next() {
+            Some(&child) => {
+                if child == usize::MAX {
+                    entry.count += 1;
+                    stack.push(entry);
+                } else {
+                    stack.push(entry);
+                    stack.push(StackEntry {
+                        count: 0,
+                        children: nodes[child].iter(),
+                    });
+                }
+            },
+            None => {
+                match stack.last_mut() {
+                    Some(last) => last.count += entry.count,
+                    None => count += entry.count,
+                }
+            },
+        }
+    }
+
+    count
+}
+
+fn count_combinations(nums: &[u32]) -> u64 {
+    count_paths(&build_tree(nums))
 }
 
 fn main() {
@@ -25,5 +78,5 @@ fn main() {
 
     nums.sort_unstable();
 
-    println!("part 2: {}", count_combinations(0, &nums));
+    println!("part 2: {}", count_combinations(&nums));
 }
