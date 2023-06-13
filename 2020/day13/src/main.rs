942dad4c512050b033bc9100a55d086bc922d8dd
Neil Roberts <bpeeluk@yahoo.co.uk>
2023-06-13 19:43:54 +0200

    2020/13: Attempt at part 2
    This works for the example but the answer is too high for the real
    input.

---
+++ 2020/day13/src/main.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Bus {
+    id: u64,
+    target_minute: u64,
+}
+
+fn read_timetable() -> Vec<Bus> {
+    let mut lines = std::io::stdin().lines();
+
+    lines.next().unwrap().unwrap();
+
+    lines.next()
+        .unwrap()
+        .unwrap()
+        .split(',')
+        .enumerate()
+        .filter_map(|(target_minute, id)| {
+            if id == "x" {
+                None
+            } else {
+                let id = id.parse().unwrap();
+                let target_minute = (id - target_minute as u64) % id;
+
+                Some(Bus { id, target_minute })
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let timetable = read_timetable();
+
+    let mut multiplier = 1;
+    let mut t = 0;
+
+    for bus in timetable.iter() {
+        println!("{:?}", bus);
+        while t % bus.id != bus.target_minute {
+            t += multiplier;
+        }
+
+        multiplier *= bus.id;
+    }
+
+    println!("part 2: {}", t);
+}
