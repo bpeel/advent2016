69546b13c5f1c4f33e58be5f29ff01c0834f223b
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-02 08:26:55 +0100

    day 1: part 2

--- 2024/day1.py
+++ 2024/day1.py
@@ -14,3 +14,20 @@ right.sort()
 diffs = map(lambda a: abs(a[0] - a[1]), zip(left, right))
 print(f"Part 1: {sum(diffs)}")
 
+counts = {}
+
+for r in right:
+    try:
+        counts[r] += 1
+    except KeyError:
+        counts[r] = 1
+
+def score(n):
+    try:
+        return counts[n] * n
+    except KeyError:
+        return 0
+
+print(f"Part 2: {sum(map(score, left))}")
+
+
