a46f67bf0ab350d23dc6e092c6001042dae34b0c
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-03 00:25:15 +0100

    2024/2: First attempt at part 2

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -54,6 +54,47 @@ fn safe(report: &[i32]) -> bool {
     true
 }
 
+fn safe2(report: &[i32]) -> bool {
+    let mut increases = Vec::new();
+    let mut decreases = Vec::new();
+    let mut other_bad = None;
+    let mut last = report[0];
+
+    for &level in &report[1..] {
+        let diff = last.abs_diff(level);
+
+        if diff < 1 || diff > 3 {
+            if other_bad.is_some() {
+                return false;
+            }
+
+            other_bad = Some(level);
+        }
+
+        if level > last {
+            increases.push(level);
+        } else if level < last {
+            decreases.push(level);
+        }
+
+        last = level;
+    }
+
+    if increases.is_empty() || decreases.is_empty() {
+        true
+    } else if increases.len() > 1 && decreases.len() > 1 {
+        false
+    } else {
+        match other_bad {
+            None => true,
+            Some(other_bad) => {
+                (decreases.len() == 1 && other_bad == decreases[0]) ||
+                    (increases.len() == 1 && other_bad == increases[0])
+            },
+        }
+    }
+}
+
 fn main() -> std::process::ExitCode {
     let reports;
 
@@ -72,6 +113,11 @@ fn main() -> std::process::ExitCode {
     println!(
         "Part 1: {:?}",
         reports.iter().map(|report| safe(report) as u32).sum::<u32>(),
+    );
+
+    println!(
+        "Part 2: {:?}",
+        reports.iter().map(|report| safe2(report) as u32).sum::<u32>(),
     );
 
     std::process::ExitCode::SUCCESS
