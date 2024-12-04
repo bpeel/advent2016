93096ba3e609d78a466e045c72a5a016b03544cd
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-04 23:18:42 +0100

    2023/1: Search the digits directly instead of using a regex

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,7 +1,10 @@
 use std::process::ExitCode;
 
+fn is_digit(ch: char) -> bool {
+    ch.is_ascii_digit()
+}
+
 fn main() -> std::process::ExitCode {
-    let re = regex::Regex::new(r"[^0-9]*([0-9]).*([0-9])").unwrap();
     let mut part1 = 0;
 
     for (line_num, line) in std::io::stdin().lines().enumerate() {
@@ -11,15 +14,18 @@ fn main() -> std::process::ExitCode {
             return ExitCode::FAILURE;
         };
 
-        let Some(captures) = re.captures(&line)
+        let Some(first) = line.find(is_digit)
         else {
-            eprintln!("line {} does not match regex", line_num + 1);
+            eprintln!("line {} contains no digit", line_num + 1);
             return ExitCode::FAILURE;
         };
 
-        let calibration_value = (captures[0].chars().next().unwrap() as u32 -
-                                 '0' as u32) * 10 +
-            (captures[1].chars().next().unwrap() as u32 - '0' as u32);
+        let last = line.rfind(is_digit).unwrap();
+
+        let calibration_value =
+            (line.as_bytes()[first] as u32 - '0' as u32) * 10 +
+            line.as_bytes()[last] as u32 -
+            '0' as u32;
 
         part1 += calibration_value;
     }
