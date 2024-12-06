191264d19aced97cf475ed7de469dd9855dd00c8
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-06 10:30:11 +0100

    2024/6: Part 2

--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -40,6 +40,58 @@ fn part1(mut grid: Grid, mut pos: (i32, i32)) -> usize {
     }
 }
 
+fn is_loop(grid: &mut Grid, mut pos: (i32, i32)) -> bool {
+    let mut direction = QuadDirection::Up;
+
+    *grid.get_mut(pos).unwrap() = b'0' + (1u8 << direction as u8);
+
+    loop {
+        let next_pos = direction.move_pos(pos);
+
+        match grid.get(next_pos) {
+            Some(b'#') => direction = direction.turn_right(),
+            Some(grid_byte) => {
+                pos = next_pos;
+
+                if grid_byte == b'.' {
+                    *grid.get_mut(pos).unwrap() =
+                        b'0' + (1u8 << direction as u8);
+                } else {
+                    // Have we already been here in the same direction?
+                    let found_loop = grid_byte & (1u8 << direction as u8) != 0;
+                    *grid.get_mut(pos).unwrap() |= 1u8 << direction as u8;
+                    if found_loop {
+                        break true;
+                    }
+                }
+            },
+            None => break false,
+        }
+    }
+}
+
+fn part2(grid: &Grid, pos: (i32, i32)) -> usize {
+    let mut count = 0;
+    let mut grid_copy = grid.clone();
+
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            if grid.values[y * grid.width + x] != b'.' {
+                continue;
+            }
+
+            grid.values.clone_into(&mut grid_copy.values);
+            grid_copy.values[y * grid.width + x] = b'#';
+
+            if is_loop(&mut grid_copy, pos) {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
 fn main() -> ExitCode {
     let grid = match Grid::load(&mut std::io::stdin().lock()) {
         Err(e) => {
@@ -56,6 +108,7 @@ fn main() -> ExitCode {
     };
 
     println!("Part 1: {}", part1(grid.clone(), start));
+    println!("Part 2: {}", part2(&grid, start));
 
     ExitCode::SUCCESS
 }
