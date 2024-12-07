3c83e4b4562f92293ec5586a097789a6e2a2a420
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-07 10:06:43 +0100

    2024/day7: First attempt
    Doesn’t work

---
+++ 2024/day7/src/main.rs
@@ -0,0 +1,123 @@
+use std::process::ExitCode;
+use std::str::FromStr;
+
+struct Equation {
+    test_value: u64,
+    numbers: Vec<u64>,
+}
+
+impl FromStr for Equation {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Equation, String> {
+        let Some((test_value_str, numbers_str)) = s.split_once(": ")
+        else {
+            return Err("missing colon".to_string());
+        };
+
+        let Ok(test_value) = test_value_str.parse::<u64>()
+        else {
+            return Err("bad test value".to_string());
+        };
+
+        let mut numbers = Vec::new();
+
+        for number in numbers_str.split_whitespace() {
+            let Ok(number) = number.parse::<u64>()
+            else {
+                return Err(format!("bad number: {}", number));
+            };
+
+            numbers.push(number);
+        }
+
+        if numbers.len() < 2 {
+            return Err("not enough numbers".to_string());
+        }
+
+        if numbers.len() > 31 {
+            return Err("too many numbers".to_string());
+        }
+
+        Ok(Equation {
+            test_value,
+            numbers,
+        })
+    }
+}
+
+fn read_equations<I>(lines: I) -> Result<Vec<Equation>, String>
+    where I: Iterator<Item = Result<String, std::io::Error>>
+{
+    let mut equations = Vec::new();
+
+    for (line_num, result) in lines.enumerate() {
+        let line = match result {
+            Err(e) => return Err(e.to_string()),
+            Ok(line) => line,
+        };
+
+        match line.parse::<Equation>() {
+            Ok(e) => equations.push(e),
+            Err(e) => return Err(format!("line {}: {}", line_num + 1, e)),
+        }
+    }
+
+    Ok(equations)
+}
+
+fn find_solution(equation: &Equation) -> Option<u32> {
+    let n_combinations = 1u32 << (equation.numbers.len() - 1);
+    let mut chosen_operators = 0;
+
+    'combination: while chosen_operators < n_combinations {
+        let mut result = equation.numbers[0];
+
+        for (i, &number) in equation.numbers[1..].iter().enumerate() {
+            if chosen_operators & (1u32 << i) == 0 {
+                result += number;
+            } else {
+                result *= number;
+            }
+
+            // If we go over the target then the rest of the numbers
+            // can only possibly make the number even bigger so
+            // there’s no point in trying any more combinations with
+            // this operator at this point.
+            if result > equation.test_value {
+                chosen_operators = (chosen_operators &
+                                    ((u32::MAX) << i)) +
+                    (1u32 << i);
+                continue 'combination;
+            }
+        }
+
+        if result == equation.test_value {
+            return Some(chosen_operators);
+        }
+
+        chosen_operators += 1;
+    }
+
+    None
+}
+
+fn part1(equations: &[Equation]) -> u64 {
+    equations.iter().filter_map(|equation| {
+        find_solution(equation).is_some().then_some(equation.test_value)
+    }).sum::<u64>()
+}
+
+fn main() -> ExitCode {
+    let equations = match read_equations(std::io::stdin().lines()) {
+        Ok(e) => e,
+        Err(e) => {
+            eprintln!("{}", e);
+            return ExitCode::FAILURE;
+        },
+    };
+
+    println!("Part 1: {}", part1(&equations));
+
+    ExitCode::SUCCESS
+}
