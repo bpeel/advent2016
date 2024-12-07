a486be4d036a83a5c0a616d5c5e7c99bbf6ab067
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-07 10:34:16 +0100

    2024/7: Part 2

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -94,6 +94,49 @@ fn part1(equations: &[Equation]) -> u64 {
     }).sum::<u64>()
 }
 
+fn elephant_operator(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        return a * 10;
+    }
+
+    let b_length = b.ilog10() + 1;
+
+    a * 10u64.pow(b_length) + b
+}
+
+fn find_elephant_solution(equation: &Equation) -> Option<u32> {
+    let n_combinations = 3u32.pow(equation.numbers.len() as u32 - 1);
+
+    for mut chosen_operators in 0..n_combinations {
+        let mut result = equation.numbers[0];
+
+        for &number in equation.numbers[1..].iter() {
+            match chosen_operators % 3 {
+                0 => result += number,
+                1 => result *= number,
+                2 => result = elephant_operator(result, number),
+                _ => unreachable!("mod 3 is broken"),
+            }
+
+            chosen_operators /= 3;
+        }
+
+        if result == equation.test_value {
+            return Some(chosen_operators);
+        }
+    }
+
+    None
+}
+
+fn part2(equations: &[Equation]) -> u64 {
+    equations.iter().filter_map(|equation| {
+        find_elephant_solution(equation)
+            .is_some()
+            .then_some(equation.test_value)
+    }).sum::<u64>()
+}
+
 fn main() -> ExitCode {
     let equations = match read_equations(std::io::stdin().lines()) {
         Ok(e) => e,
@@ -104,6 +147,20 @@ fn main() -> ExitCode {
     };
 
     println!("Part 1: {}", part1(&equations));
+    println!("Part 2: {}", part2(&equations));
 
     ExitCode::SUCCESS
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn elephant() {
+        assert_eq!(elephant_operator(10, 0), 100);
+        assert_eq!(elephant_operator(1, 2), 12);
+        assert_eq!(elephant_operator(999, 9), 9999);
+        assert_eq!(elephant_operator(123456789, 56), 12345678956);
+    }
 }
