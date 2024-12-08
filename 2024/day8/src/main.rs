e4230ec58f56eb10042881925ca05dd9dee2d445
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-08 19:24:06 +0100

    2024/8: Part 2

--- 2024/day8/src/main.rs
+++ 2024/day8/src/main.rs
@@ -39,6 +39,37 @@ fn find_antinodes(antennas: &AntennaMap) -> HashSet<(i32, i32)> {
     antinodes
 }
 
+fn add_harmonics(
+    grid: &Grid,
+    mut pos: (i32, i32),
+    dir: (i32, i32),
+    antinodes: &mut HashSet<(i32, i32)>,
+) {
+    while grid.get(pos).is_some() {
+        antinodes.insert(pos);
+
+        pos = (pos.0 + dir.0, pos.1 + dir.1);
+    }
+}
+
+fn find_antinodes_with_harmonics(
+    grid: &Grid,
+    antennas: &AntennaMap,
+) -> HashSet<(i32, i32)> {
+    let mut antinodes = HashSet::new();
+
+    for antennas in antennas.values() {
+        for (pos, &a) in antennas.iter().enumerate() {
+            for &b in &antennas[pos + 1..] {
+                add_harmonics(grid, a, (b.0 - a.0, b.1 - a.1), &mut antinodes);
+                add_harmonics(grid, b, (a.0 - b.0, a.1 - b.1), &mut antinodes);
+            }
+        }
+    }
+
+    antinodes
+}
+
 fn print_antinodes<'a, I>(
     grid: &Grid,
     antinodes: I,
@@ -68,9 +99,13 @@ fn main() -> std::process::ExitCode {
 
     antinodes.retain(|&pos| grid.get(pos).is_some());
 
-    print_antinodes(&grid, &antinodes);
+    let antinodes_with_harmonics =
+        find_antinodes_with_harmonics(&grid, &antennas);
+
+    print_antinodes(&grid, &antinodes_with_harmonics);
 
     println!("Part 1: {}", antinodes.len());
+    println!("Part 2: {}", antinodes_with_harmonics.len());
 
     ExitCode::SUCCESS
 }
