1d1f2fa42c50f8d915b1e953b0ff04af6e4bd075
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-18 13:51:02 +0100

    2024/18: Naïve attempt at part 2
    I don’t think this will complete in a reasonable time.

--- 2024/day18/src/main.rs
+++ 2024/day18/src/main.rs
@@ -43,6 +43,38 @@ fn get_grid_size() -> Result<(i32, i32), String> {
     Ok((parts[0], parts[1]))
 }
 
+fn find_solution(
+    grid_size: (i32, i32),
+    bytes: &HashMap<(i32, i32), usize>,
+    bytes_dropped: usize,
+) -> Option<usize> {
+    let mut best = None;
+
+    walker::shortest_walk::<QuadDirection, _>((0, 0), |path, pos| {
+        if pos.0 < 0 || pos.0 >= grid_size.0 ||
+            pos.1 < 0 || pos.1 >= grid_size.1
+        {
+            return VisitResult::Backtrack;
+        }
+
+        if bytes.get(&pos)
+            .map(|&index| index < bytes_dropped)
+            .unwrap_or(false)
+        {
+            return VisitResult::Backtrack;
+        }
+
+        if pos == (grid_size.0 - 1, grid_size.1 - 1) {
+            best = Some(path.len());
+            VisitResult::Goal
+        } else {
+            VisitResult::Continue
+        }
+    });
+
+    best
+}
+
 fn main() -> ExitCode {
     let bytes = match read_bytes() {
         Ok(bytes) => bytes,
@@ -60,43 +92,24 @@ fn main() -> ExitCode {
         },
     };
 
-    let mut best = None;
-
-    for y in 0..grid_size.1 {
-        for x in 0..grid_size.0 {
-            if bytes.get(&(x, y)).map(|&index| index < 1024).unwrap_or(false) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-
-        println!();
-    }
-
-    walker::shortest_walk::<QuadDirection, _>((0, 0), |path, pos| {
-        if pos.0 < 0 || pos.0 >= grid_size.0 ||
-            pos.1 < 0 || pos.1 >= grid_size.1
-        {
-            return VisitResult::Backtrack;
-        }
-
-        if bytes.get(&pos).map(|&index| index < 1024).unwrap_or(false) {
-            return VisitResult::Backtrack;
-        }
-
-        if pos == (grid_size.0 - 1, grid_size.1 - 1) {
-            best = Some(path.len());
-            VisitResult::Goal
-        } else {
-            VisitResult::Continue
-        }
-    });
+    let best = find_solution(grid_size, &bytes, 1024);
 
     println!(
         "Part 1: {}",
         best.map(|l| l.to_string()).unwrap_or("no path found".to_string()),
     );
+
+    for bytes_dropped in 1..=bytes.len() {
+        if find_solution(grid_size, &bytes, bytes_dropped).is_none() {
+            let first_bad_byte = bytes.iter().find(|(_, &index)| {
+                index == bytes_dropped - 1
+            }).unwrap().0;
+
+            println!("Part 2: {},{}", first_bad_byte.0, first_bad_byte.1);
+
+            break;
+        }
+    }
 
     ExitCode::SUCCESS
 }
