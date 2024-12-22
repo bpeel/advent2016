eb05ce4ba4b9dfcf1193f97ebd00f290865114ba
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-12-22 11:14:03 +0100

    2024/22: Attempt at brute forcing part 2

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -13,12 +13,42 @@ fn part1(numbers: &[u64]) {
     let result = numbers.iter().map(|&number| {
         let secret = (0..2000).fold(number, |number, _| step(number));
 
-        println!("{}: {}", number, secret);
-
         secret
     }).sum::<u64>();
 
     println!("Part 1: {}", result);
+}
+
+fn add_to_history(number: u64, history: &mut [u8; 5]) {
+    history.copy_within(1.., 0);
+    history[4] = (number % 10) as u8;
+}
+
+fn history_matches_pattern(history: [u8; 5], pattern: [i8; 4]) -> bool {
+    (1..=4).all(|pos| {
+        history[pos] as i8 - history[pos - 1] as i8 ==
+            pattern[pos - 1]
+    })
+}
+
+fn find_pattern(mut number: u64, pattern: [i8; 4]) -> Option<u8> {
+    let mut history = [0u8; 5];
+
+    for _ in 0..4 {
+        add_to_history(number, &mut history);
+        number = step(number);
+    }
+
+    for _ in 0..2000 - 4 {
+        add_to_history(number, &mut history);
+        number = step(number);
+
+        if history_matches_pattern(history, pattern) {
+            return Some(*history.last().unwrap());
+        }
+    }
+
+    None
 }
 
 fn read_numbers() -> Result<Vec<u64>, String> {
@@ -27,6 +57,42 @@ fn read_numbers() -> Result<Vec<u64>, String> {
 
         line.parse::<u64>().map_err(|_| format!("invalid number: {}", line))
     }).collect()
+}
+
+struct Patterns {
+    pattern: [i8; 4],
+}
+
+impl Patterns {
+    fn new() -> Patterns {
+        Patterns {
+            pattern: [-10, -9, -9, -9],
+        }
+    }
+}
+
+impl Iterator for Patterns {
+    type Item = [i8; 4];
+
+    fn next(&mut self) -> Option<[i8; 4]> {
+        for pos in self.pattern.iter_mut() {
+            *pos += 1;
+
+            if *pos <= 9 {
+                return Some(self.pattern);
+            } else {
+                *pos = -9;
+            }
+        }
+
+        None
+    }
+}
+
+fn score_pattern(numbers: &[u64], pattern: [i8; 4]) -> u64 {
+    numbers.iter().map(|&number| {
+        find_pattern(number, pattern).unwrap_or(0) as u64
+    }).sum::<u64>()
 }
 
 fn main() -> ExitCode {
@@ -39,6 +105,13 @@ fn main() -> ExitCode {
     };
 
     part1(&numbers);
+
+    let part2 = Patterns::new()
+        .map(|pattern| score_pattern(&numbers, pattern))
+        .max()
+        .unwrap();
+
+    println!("Part 2: {:?}", part2);
 
     ExitCode::SUCCESS
 }
